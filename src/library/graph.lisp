e89874da840033e82a3ad659fc4e89e55e85dc9a
Cole Scott <cwscott@hrl.com>
2021-09-06 12:25:47 -0700

    Add tarjan scc to graph library

--- src/library/graph.lisp
+++ src/library/graph.lisp
@@ -193,9 +193,9 @@
   (declare make-digraph (Unit -> (Graph :node-data :edge-data)))
   (define (make-digraph _)
     "Create a new directed graph"
-    (Graph Undirected
-	   (make-vector Unit)
-	   (make-vector Unit)))
+    (Graph Directed
+           (make-vector Unit)
+           (make-vector Unit)))
 
   (declare graph-nodes ((Graph :node-data :edge-data) -> (Vector (Node :node-data))))
   (define (graph-nodes graph)
@@ -244,8 +244,8 @@
     (match graph
       ((Graph _ nodes _)
        (progn
-	 (vector-push (Node node-data (IndexPair None None)) nodes)
-	 (NodeIndex (vector-length nodes))))))
+         (vector-push (Node node-data (IndexPair None None)) nodes)
+         (NodeIndex (- (vector-length nodes) 1))))))
 
   (declare graph-remove-node (NodeIndex -> (Graph :node-data :edge-data) -> (Optional :node-data)))
   (define (graph-remove-node index graph)
@@ -262,60 +262,60 @@
                        (remove-edges idx accessor))))))))
       (do (node_ <- (vector-index (into index) (graph-nodes graph)))
           (remove-edges (into index) index-pair-incoming)
-          (remove-edges (into index) index-pair-outgoing)
-          (node_ <- (vector-swap-remove (into index) (graph-nodes graph)))
-          (match (vector-index (into index) (graph-nodes graph))
-            ((None)
-             (Some (node-data node_)))
-            ((Some ed)
-             (let ((swap-edges (node-edge-pair ed))
-                   (old-index (NodeIndex (vector-length (graph-nodes graph))))
-                   (accessor index-pair-incoming)
+        (remove-edges (into index) index-pair-outgoing)
+        (node_ <- (vector-swap-remove (into index) (graph-nodes graph)))
+        (match (vector-index (into index) (graph-nodes graph))
+          ((None)
+           (Some (node-data node_)))
+          ((Some ed)
+           (let ((swap-edges (node-edge-pair ed))
+                 (old-index (NodeIndex (vector-length (graph-nodes graph))))
+                 (accessor index-pair-incoming)
 
-                   (adjust-edges
-                     (fn (idx dir-node-accessor dir-edge-accessor dir-update)
-                       (match (graph-lookup-edge idx graph)
-                         ((None)
-                          None)
-                         ((Some e)
-                          (progn
-                            (unless (== (dir-node-accessor (edge-node-pair e))
-                                        old-index)
-                              (error "Found edge with incorrect node index"))
+                 (adjust-edges
+                   (fn (idx dir-node-accessor dir-edge-accessor dir-update)
+                     (match (graph-lookup-edge idx graph)
+                       ((None)
+                        None)
+                       ((Some e)
+                        (progn
+                          (unless (== (dir-node-accessor (edge-node-pair e))
+                                      old-index)
+                            (error "Found edge with incorrect node index"))
 
-                            (vector-set (into idx)
-                                        (Edge (edge-data e)
-                                              (edge-next-pair e)
-                                              (dir-update (edge-node-pair e) index))
-                                        (graph-edges graph))
+                          (vector-set (into idx)
+                                      (Edge (edge-data e)
+                                            (edge-next-pair e)
+                                            (dir-update (edge-node-pair e) index))
+                                      (graph-edges graph))
 
-                            (match (dir-edge-accessor (edge-next-pair e))
-                              ((None)
-                               None)
-                              ((Some next-idx)
-                               (adjust-edges next-idx dir-node-accessor dir-edge-accessor dir-update)))))))))
-               (progn
-                 (match (index-pair-incoming swap-edges)
-                   ((None) None)
-                   ((Some edge)
-                    (adjust-edges edge
-                                  index-pair-incoming
-                                  index-pair-incoming
-                                  (fn (pair val)
-                                    (IndexPair (index-pair-outgoing pair)
-                                               val)))))
+                          (match (dir-edge-accessor (edge-next-pair e))
+                            ((None)
+                             None)
+                            ((Some next-idx)
+                             (adjust-edges next-idx dir-node-accessor dir-edge-accessor dir-update)))))))))
+             (progn
+               (match (index-pair-incoming swap-edges)
+                 ((None) None)
+                 ((Some edge)
+                  (adjust-edges edge
+                                index-pair-incoming
+                                index-pair-incoming
+                                (fn (pair val)
+                                  (IndexPair (index-pair-outgoing pair)
+                                             val)))))
 
-                 (match (index-pair-outgoing swap-edges)
-                   ((None) None)
-                   ((Some edge)
-                    (adjust-edges edge
-                                  index-pair-outgoing
-                                  index-pair-outgoing
-                                  (fn (pair val)
-                                    (IndexPair val
-                                               (index-pair-incoming pair))))))
+               (match (index-pair-outgoing swap-edges)
+                 ((None) None)
+                 ((Some edge)
+                  (adjust-edges edge
+                                index-pair-outgoing
+                                index-pair-outgoing
+                                (fn (pair val)
+                                  (IndexPair val
+                                             (index-pair-incoming pair))))))
 
-                 (Some (node-data node_)))))))))
+               (Some (node-data node_)))))))))
 
 
   (declare graph-add-edge (:edge-data -> NodeIndex -> NodeIndex -> (Graph :node-data :edge-data) -> EdgeIndex))
@@ -332,35 +332,35 @@
 
         (if (== from to)
             ;; If FROM == TO then we are creating a self-cycle
-	    (progn
+            (progn
               ;; Pull the exiting node out of the graph
-	      (let node_ = (fromSome "unreachable" (graph-lookup-node from graph)))
+              (let node_ = (fromSome "unreachable" (graph-lookup-node from graph)))
               ;; Construct the edge, pointing to the previous heads of the edge lists
-	      (let edge_ = (Edge edge-data
+              (let edge_ = (Edge edge-data
                                  (node-edge-pair node_)
                                  (IndexPair from to)))
               ;; Construct a new node with the new edge as the head of the incoming/outgoing edge lists
-	      (let node__ = (Node (node-data node_)
+              (let node__ = (Node (node-data node_)
                                   (IndexPair (Some edge-idx) (Some edge-idx))))
 
               ;; Finally, add the edge and replace the node
-	      (vector-push edge_ (graph-edges graph))
-	      (vector-set (into from) node__ (graph-nodes graph))
+              (vector-push edge_ (graph-edges graph))
+              (vector-set (into from) node__ (graph-nodes graph))
               ;; And return the index of the new edge
-	      edge-idx)
+              edge-idx)
 
             ;; Otherwise, create non-cycle (using two nodes)
             (progn
               ;; Pull the exiting nodes out of the graph
-	      (let from-node = (fromSome "unreachable" (graph-lookup-node from graph)))
+              (let from-node = (fromSome "unreachable" (graph-lookup-node from graph)))
               (let to-node   = (fromSome "unreachable" (graph-lookup-node to graph)))
               ;; Construct the edge, pointing to the previous heads of the edge lists
-	      (let edge_ = (Edge edge-data
+              (let edge_ = (Edge edge-data
                                  (IndexPair (index-pair-outgoing (node-edge-pair from-node))
                                             (index-pair-incoming (node-edge-pair to-node)))
                                  (IndexPair from to)))
               ;; Construct new nodes with the new edge as the head of the incoming/outgoing edge lists
-	      (let from-node_ = (Node (node-data from-node)
+              (let from-node_ = (Node (node-data from-node)
                                       (IndexPair (Some edge-idx)
                                                  (index-pair-incoming (node-edge-pair from-node)))))
               (let to-node_   = (Node (node-data to-node)
@@ -368,18 +368,18 @@
                                                  (Some edge-idx))))
 
               ;; Finally, add the edge and replace the nodes
-	      (vector-push edge_ (graph-edges graph))
-	      (vector-set (into from) from-node_ (graph-nodes graph))
+              (vector-push edge_ (graph-edges graph))
+              (vector-set (into from) from-node_ (graph-nodes graph))
               (vector-set (into to) to-node_ (graph-nodes graph))
               ;; And return the index of the new edge
-	      edge-idx)))))
+              edge-idx)))))
 
   (declare graph-remove-edge (EdgeIndex -> (Graph :node-data :edge-data) -> (Optional :edge-data)))
   (define (graph-remove-edge index graph)
     "Remove an edge from GRAPH"
     (do (edge <- (vector-index (into index) (graph-edges graph)))
         (let _ = (change-edge-links (edge-node-pair edge) index (edge-next-pair edge) graph))
-        (remove-edge-adjust-indices index graph)))
+      (remove-edge-adjust-indices index graph)))
 
   ;; Helpers for above function
 
@@ -454,37 +454,183 @@
                (change-edge-links swap swapped-edge (IndexPair (Some index) (Some index)) graph)
                (Some (edge-data edge_))))))))
 
+  ;; TODO: add function to output graphviz where subgraphs are sccs
   (declare graph-viz (Show :node-data => ((Graph :node-data :edge-data) -> String)))
   (define (graph-viz graph_)
     (progn
       (let elements = (make-vector Unit))
       (if (graph-is-directed graph_)
-	  (vector-push "digraph {" elements)
-	  (vector-push "graph {" elements))
+          (vector-push "digraph {" elements)
+          (vector-push "graph {" elements))
       (vector-foreach-index
        (fn (i node)
-	(vector-push
-	  (msum
-	  (make-list
-	    (show i)
-	    " [label=\""
-	    (show (node-data node))
-	    "\"]"))
-	  elements))
+         (vector-push
+          (msum
+           (make-list
+            (show i)
+            " [label=\""
+            (show (node-data node))
+            "\"]"))
+          elements))
        (graph-nodes graph_))
       (vector-foreach
        (fn (edge)
-	(vector-push
-	  (msum
-	  (make-list 
-	    (show (edge-from-index edge))
-	    (if (graph-is-directed graph_)
-		" -> "
-		" -- ")
-	    (show (edge-to-index edge))))
-	  elements))
+         (vector-push
+          (msum
+           (make-list
+            (show (edge-from-index edge))
+            (if (graph-is-directed graph_)
+                " -> "
+                " -- ")
+            (show (edge-to-index edge))))
+          elements))
        (graph-edges graph_))
       (vector-push "}" elements)
-      (msum (intersperse (show #\NEWLINE) (vector-to-list elements))))))
+      (msum (intersperse (show #\NEWLINE) (vector-to-list elements)))))
 
 
+  ;;
+  ;; Tarjan strongly connected components algorithm
+  ;;
+
+  (define-type TarjanNode
+    (TarjanNode (Optional Int) Int Boolean))
+
+  (declare tarjan-node-index (TarjanNode -> (Optional Int)))
+  (define (tarjan-node-index node)
+    (match node
+      ((TarjanNode index _ _)
+       index)))
+
+  (declare tarjan-node-low-link (TarjanNode -> Int))
+  (define (tarjan-node-low-link node)
+    (match node
+      ((TarjanNode _ low-link _)
+       low-link)))
+
+  (declare tarjan-node-on-stack (TarjanNode -> Boolean))
+  (define (tarjan-node-on-stack node)
+    (match node
+      ((TarjanNode _ _ on-stack)
+       on-stack)))
+
+
+  (declare tarjan-scc ((Graph :node-data :edge-data) -> (List (List NodeIndex))))
+  (define (tarjan-scc graph)
+    (let ((current-index (make-cell 0))
+          (tarjan-data (make-vector Unit))
+          (stack (make-vector Unit))
+          (output-sccs (make-vector Unit)))
+
+      (progn
+        ;; Pre-populate the tarjan data
+        (vector-foreach-index
+         (fn (i x)
+           (vector-push (TarjanNode None i False) tarjan-data))
+         (graph-nodes graph))
+
+        (let visit-node =
+          (fn (node-idx)
+            ;; Only act on unvisited nodes
+            (unless (isSome (tarjan-node-index (vector-index-unsafe node-idx tarjan-data)))
+              (progn
+                ;; Update the tarjan entry
+                (vector-set node-idx
+                            (TarjanNode (Some (cell-read current-index))
+                                        (cell-read current-index)
+                                        True)
+                            tarjan-data)
+
+                ;; Push it on to the stack
+                (vector-push node-idx stack)
+
+                ;; Increment the current index
+                (cell-write (+ 1 (cell-read current-index)) current-index)
+
+                ;; Consider successors
+                (let visit-edge =
+                  (fn (edge-idx)
+                    (let ((edge-entry (vector-index-unsafe edge-idx (graph-edges graph)))
+                          (w (+ 0 (into (edge-to-index edge-entry))))
+                          (w-entry (vector-index-unsafe w tarjan-data))
+                          (w-index (tarjan-node-index w-entry)))
+                      (progn
+                        (cond
+                          ((isNone w-index)
+                           (progn
+                             ;; The successor has not been visited. Visit it
+                             (visit-node w)
+
+                             ;; Update the lowlink of the successor
+                             (let w-entry = (vector-index-unsafe w tarjan-data))
+                             (let v-entry = (vector-index-unsafe node-idx tarjan-data))
+                             (vector-set
+                              node-idx
+                              (TarjanNode
+                               (tarjan-node-index v-entry)
+                               (min (tarjan-node-low-link w-entry)
+                                    (tarjan-node-low-link v-entry))
+                               (tarjan-node-on-stack v-entry))
+                              tarjan-data)
+                             Unit))
+                          ((tarjan-node-on-stack w-entry)
+                           (progn
+                             (let w-entry = (vector-index-unsafe w tarjan-data))
+                             (let v-entry = (vector-index-unsafe node-idx tarjan-data))
+                             (vector-set
+                              node-idx
+                              (TarjanNode
+                               (tarjan-node-index v-entry)
+                               (min (fromSome "unreachable" (tarjan-node-index w-entry))
+                                    (tarjan-node-low-link v-entry))
+                               (tarjan-node-on-stack v-entry))
+                              tarjan-data)
+
+                             Unit))
+                          (True
+                           Unit))
+
+                        (match (index-pair-outgoing (edge-next-pair edge-entry))
+                          ((None)
+                           Unit)
+                          ((Some (EdgeIndex idx))
+                           (visit-edge idx)))))))
+
+                ;; Visit the first edge for the node
+                (match (index-pair-outgoing (node-edge-pair (vector-index-unsafe node-idx (graph-nodes graph))))
+                  ((None)
+                   Unit)
+                  ((Some (EdgeIndex idx))
+                   (visit-edge idx)))
+
+                (let v-entry = (vector-index-unsafe node-idx tarjan-data))
+                (when (== (tarjan-node-low-link v-entry)
+                          (fromSome "unreachable" (tarjan-node-index v-entry)))
+                  (let ((build-scc
+                          (fn (scc)
+                            (let ((w (vector-pop-unsafe stack)))
+                              (progn
+                                ;; Update W to not be on the stack
+                                (let w-entry = (vector-index-unsafe w tarjan-data))
+                                (vector-set
+                                 w
+                                 (TarjanNode
+                                  (tarjan-node-index w-entry)
+                                  (tarjan-node-low-link w-entry)
+                                  False)
+                                 tarjan-data)
+
+                                ;; And recurse
+                                (if (== w node-idx)
+                                    (Cons (NodeIndex w) scc)
+                                    (build-scc (Cons (NodeIndex w) scc))))))))
+                    ;; Build and emit the scc
+                    (vector-push (build-scc Nil) output-sccs)))
+                Unit))))
+
+        (vector-foreach-index
+         (fn (i _)
+           (visit-node i))
+         (graph-nodes graph))
+
+        (vector-to-list output-sccs)))))
