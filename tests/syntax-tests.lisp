13dee2cdf7330afc8188920da84b0b3605de5b6f
Daniel Koning <dk@danielkoning.com>
2021-12-13 11:17:58 -0600

    Test codewalking with a separate utility function
    We don't want problems with the typechecker to manifest as apparent
    problems with the codewalker.

--- tests/syntax-tests.lisp
+++ tests/syntax-tests.lisp
@@ -2,35 +2,35 @@
 
 (deftest test-repr-form-position ()
   (signals coalton-impl::coalton-parse-error
-    (run-coalton-typechecker
+    (run-coalton-walker
      ;; No next form
      '((coalton:repr :lisp))))
   (signals coalton-impl::coalton-parse-error
-    (run-coalton-typechecker
+    (run-coalton-walker
      ;; Wrong next form
      '((coalton:repr :lisp)
        (coalton:define foo "foo")))))
 
 (deftest test-repr-form-arity ()
   (signals coalton-impl::coalton-parse-error
-    (run-coalton-typechecker
+    (run-coalton-walker
      ;; Too few arguments
      '((coalton:repr)
        (coalton:define-type Foo Foo))))
   (signals coalton-impl::coalton-parse-error
-    (run-coalton-typechecker
+    (run-coalton-walker
      ;; Too many arguments
      '((coalton:repr :lisp :what-am-i-doing-here)
        (coalton:define-type Foo Foo)))))
 
 (deftest test-repr-form-argument ()
   (signals warning
-    (run-coalton-typechecker
+    (run-coalton-walker
      ;; Not a meaningful repr choice
      '((coalton:repr :something-weird)
        (coalton:define-type Foo Foo))))
   (finishes
-    (run-coalton-typechecker
+    (run-coalton-walker
      ;; Perfect!
      '((coalton:repr :lisp)
        (coalton:define-type Foo Foo)))))
