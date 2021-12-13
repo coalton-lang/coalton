b9728d2bf780427f261230ef103d40935345227e
Daniel Koning <dk@danielkoning.com>
2021-12-13 12:33:48 -0600

    Improve nomenclature for toplevel codewalker tests
    Be specific about the fact that the utility function only tests the
    *toplevel* codewalker. Also change the file name of the test suite
    better to reflect the scope of what it tests.

--- tests/toplevel-walker-tests.lisp
+++ tests/toplevel-walker-tests.lisp
@@ -2,57 +2,57 @@
 
 (deftest test-bad-toplevel-type ()
   (signals coalton-impl::coalton-parse-error
-    (run-coalton-walker
+    (run-coalton-toplevel-walker
      ;; Atoms don't make sense at toplevel.
      '(:this-is-a-stray-atom)))
   (signals coalton-impl::coalton-parse-error
-    (run-coalton-walker
+    (run-coalton-toplevel-walker
      ;; Neither do lists with cars that are not symbols.
      '((1729)))))
 
 (deftest test-bad-toplevel-form ()
   (signals coalton-impl::coalton-parse-error
-    (run-coalton-walker
+    (run-coalton-toplevel-walker
      ;; Expressions representing values don't make sense there either.
      '((coalton:Cons coalton:Nil coalton:Nil)))))
 
 (deftest test-repr-form-position ()
   (signals coalton-impl::coalton-parse-error
-    (run-coalton-walker
+    (run-coalton-toplevel-walker
      ;; No next form
      '((coalton:repr :lisp))))
   (signals coalton-impl::coalton-parse-error
-    (run-coalton-walker
+    (run-coalton-toplevel-walker
      ;; Wrong next form
      '((coalton:repr :lisp)
        (coalton:define foo "foo")))))
 
 (deftest test-repr-form-arity ()
   (signals coalton-impl::coalton-parse-error
-    (run-coalton-walker
+    (run-coalton-toplevel-walker
      ;; Too few arguments
      '((coalton:repr)
        (coalton:define-type Foo Foo))))
   (signals coalton-impl::coalton-parse-error
-    (run-coalton-walker
+    (run-coalton-toplevel-walker
      ;; Too many arguments
      '((coalton:repr :lisp :what-am-i-doing-here)
        (coalton:define-type Foo Foo)))))
 
 (deftest test-repr-form-argument ()
   (signals style-warning
-    (run-coalton-walker
+    (run-coalton-toplevel-walker
      ;; Not a meaningful repr choice
      '((coalton:repr :something-weird)
        (coalton:define-type Foo Foo))))
   (finishes
-    (run-coalton-walker
+    (run-coalton-toplevel-walker
      ;; Perfect!
      '((coalton:repr :lisp)
        (coalton:define-type Foo Foo)))))
 
-(deftest test-walker-return-value ()
-  (let ((trivial-value (run-coalton-walker '())))
+(deftest test-toplevel-walker-return-value ()
+  (let ((trivial-value (run-coalton-toplevel-walker '())))
     (is (listp trivial-value))
     (is (hash-table-p
          (getf trivial-value 'coalton-impl::repr-table)))))
