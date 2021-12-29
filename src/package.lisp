19f1402e7d0bbc525ebad62a7b666cca4e15a96a
Cole Scott <me@colescott.io>
2021-12-28 20:29:31 -0800

    Minor changes to documentation generator
    - Move documentation generator in to asdf system coalton/doc
    - Pass markdown through html encoding
    - Move markdown-specific code in to markdown file

--- src/package.lisp
+++ src/package.lisp
@@ -337,17 +337,6 @@
    #:*emit-type-annotations*
    ))
 
-(uiop:define-package #:coalton-impl/doc
-  (:documentation "Implementation of documentation generation for COALTON. This is a package private to the COALTON system and is not intended for public use.")
-  (:use #:cl
-        #:coalton-util
-        #:coalton-impl/algorithm
-        #:coalton-impl/ast
-        #:coalton-impl/typechecker
-        #:coalton-impl/codegen)
-  (:export
-   #:write-documentation-for-packages))
-
 (uiop:define-package #:coalton-impl
   (:documentation "Implementation and runtime for COALTON. This is a package private to the COALTON system and is not intended for public use.")
   (:use #:cl
