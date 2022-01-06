3892143af912ef2999836bdfc16ce5eef69bac9e
Elias Lawson-Fox <me@eliaslfox.com>
2022-01-05 17:59:09 -0800

    Add hash instances for tuples

--- src/library/tuple.lisp
+++ src/library/tuple.lisp
@@ -38,6 +38,14 @@
 
   (define-instance (Iso (Tuple :a :b) (Tuple :b :a)))
 
+  (define-instance ((Hash :a) (Hash :b) => (Hash (Tuple :a :b)))
+    (define (hash item)
+      (match item
+        ((Tuple a b)
+         (combine-hashes
+          (hash a)
+          (hash b))))))
+
   ;;
   ;; Larger Tuple Instances
   ;;
@@ -51,6 +59,16 @@
               (== b1 b2)
               (== c1 c2))))))
 
+  (define-instance ((Hash :a) (Hash :b) (Hash :c) => (Hash (Tuple3 :a :b :c)))
+    (define (hash item)
+      (match item
+        ((Tuple3 a b c)
+         (combine-hashes
+          (hash a)
+          (combine-hashes
+           (hash b)
+           (hash c)))))))
+
   (define-instance ((Eq :a) (Eq :b) (Eq :c) (Eq :d) => (Eq (Tuple4 :a :b :c :d)))
     (define (== a b)
       (match (Tuple a b)
@@ -61,6 +79,18 @@
               (== c1 c2)
               (== d1 d2))))))
 
+  (define-instance ((Hash :a) (Hash :b) (Hash :c) (Hash :d) => (Hash (Tuple4 :a :b :c :d)))
+    (define (hash item)
+      (match item
+        ((Tuple4 a b c d)
+         (combine-hashes
+          (hash a)
+          (combine-hashes
+           (hash b)
+           (combine-hashes
+            (hash c)
+            (hash d))))))))
+
   (define-instance ((Eq :a) (Eq :b) (Eq :c) (Eq :d) (Eq :e) => (Eq (Tuple5 :a :b :c :d :e)))
     (define (== a b)
       (match (Tuple a b)
@@ -70,4 +100,23 @@
               (== b1 b2)
               (== c1 c2)
               (== d1 d2)
-              (== e1 e2)))))))
+              (== e1 e2))))))
+
+  (define-instance ((Hash :a)
+                    (Hash :b)
+                    (Hash :c)
+                    (Hash :d)
+                    (Hash :e)
+                    => (Hash (Tuple5 :a :b :c :d :e)))
+    (define (hash item)
+      (match item
+        ((Tuple5 a b c d e)
+         (combine-hashes
+          (hash a)
+          (combine-hashes
+           (hash b)
+           (combine-hashes
+            (hash c)
+            (combine-hashes
+             (hash d)
+             (hash e))))))))))
