8f0e9724a598afbce11f750800deed285e6e4d54
Elias Lawson-Fox <me@eliaslfox.com>
2022-01-12 16:30:38 -0800

    Rearrange type definitions

--- src/library/builtin.lisp
+++ src/library/builtin.lisp
@@ -45,4 +45,40 @@
   (declare flip ((:a -> :b -> :c) -> :b -> :a -> :c))
   (define (flip f x y)
     "Returns a function that takes its arguments in reverse order."
-    (f y x)))
+    (f y x))
+
+  (define not
+    "Synonym for BOOLEAN-NOT."
+    boolean-not)
+
+  (define xor
+    "Synonym for BOOLEAN-XOR."
+    boolean-xor)
+
+  (declare boolean-not (Boolean -> Boolean))
+  (define (boolean-not x)
+    "Is X False?"
+    (match x
+      ((True) False)
+      ((False) True)))
+
+  (declare boolean-or (Boolean -> Boolean -> Boolean))
+  (define (boolean-or x y)
+    "Is X or Y True? Note that this is a *function* which means both X and Y will be evaluated. Use the OR macro for short-circuiting behavior."
+    (match x
+      ((True) True)
+      ((False) y)))
+
+  (declare boolean-and (Boolean -> Boolean -> Boolean))
+  (define (boolean-and x y)
+    "Are X and Y True? Note that this is a *function* which means both X and Y will be evaluated. Use the AND macro for short-circuiting behavior."
+    (match x
+      ((True) y)
+      ((False) False)))
+
+  (declare boolean-xor (Boolean -> Boolean -> Boolean))
+  (define (boolean-xor x y)
+    "Are X or Y True, but not both?"
+    (match x
+      ((True) (boolean-not y))
+      ((False) y))))
