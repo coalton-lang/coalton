f677cb646cadc8d0cc961a8ec39bb70187fe17fd
Phoebe Goldman <phoebe@goldman-tribe.org>
2022-01-27 11:00:27 -0500

    forgot to commit the two test files

---
+++ tests/coalton-native-test-utils.lisp
@@ -0,0 +1,57 @@
+(cl:in-package #:coalton-native-tests)
+
+(cl:defmacro define-test (name args cl:&body (body))
+  `(fiasco:deftest (,name :in fiasco-suites::coalton-tests)
+       ,args
+     (coalton
+      ,body)))
+
+(cl:define-condition coalton-is-assertion (fiasco::test-assertion)
+  ((form :initarg :form
+         :accessor coalton-is-assertion-form))
+  (:documentation "Signaled before evaluating an IS assertion"))
+
+(cl:define-condition coalton-failed-assertion (fiasco::failure)
+  ((form :initarg :form
+         :accessor coalton-is-assertion-form))
+  (:documentation "Signaled when an IS assertion fails"))
+
+(coalton-toplevel
+  (declare %register-assertion (Lisp-Object -> Unit))
+  (define (%register-assertion form)
+    (progn 
+      (lisp :any (form)
+        (cl:warn 'coalton-is-assertion
+                 :form form))
+      Unit))
+
+  (declare %register-success (Unit -> Unit))
+  (define (%register-success _)
+    (progn 
+      (lisp :any ()
+        (fiasco::register-assertion-was-successful))
+      Unit))
+
+  (declare %register-failure (Lisp-Object -> String -> Unit))
+  (define (%register-failure form message)
+    (progn
+      (lisp :any (form message)
+        (fiasco::record-failure 'coalton-failed-assertion
+                                :form form
+                                :format-control "~a"
+                                :format-arguments message))
+      Unit))
+
+  (declare %is (Lisp-Object -> (Unit -> Boolean) -> String -> Unit))
+  (define (%is form ok? message)
+    (progn
+      (%register-assertion form)
+      (if (ok?)
+          (%register-success)
+          (%register-failure form message))
+      Unit)))
+
+(cl:defmacro is (check cl:&optional (message (cl:format cl:nil "assertion failed: ~a" check)))
+  `(%is (lisp Lisp-Object () ',check)
+        (fn (_) ,check)
+        ,message))
