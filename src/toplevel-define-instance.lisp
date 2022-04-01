a946f5a4c7e105d1683cea165ccf042080e225d5
Elias Lawson-Fox <ejlawsonfox@hrl.com>
2022-04-01 14:54:03 -0700

    Register instance constructor functions
    Fixes #460

--- src/toplevel-define-instance.lisp
+++ src/toplevel-define-instance.lisp
@@ -17,47 +17,55 @@
            (type environment env)
            (values environment))
 
-    (loop :for form :in definstance-forms
-          :do (multiple-value-bind (predicate context methods)
-                  (coalton-impl/typechecker::parse-instance-decleration form env)
-                (declare (ignore methods))
-                (let* ((class-name (ty-predicate-class predicate))
+  (loop :for form :in definstance-forms
+        :do (multiple-value-bind (predicate context methods)
+                (coalton-impl/typechecker::parse-instance-decleration form env)
+              (declare (ignore methods))
+              (let* ((class-name (ty-predicate-class predicate))
 
-                       (instance-codegen-sym
-                         (alexandria:format-symbol
-                          package "INSTANCE/~A"
-                          (with-output-to-string (s)
-                            (with-pprint-variable-context ()
-                              (pprint-predicate s predicate)))))
+                     (instance-codegen-sym
+                       (alexandria:format-symbol
+                        package "INSTANCE/~A"
+                        (with-output-to-string (s)
+                          (with-pprint-variable-context ()
+                            (pprint-predicate s predicate)))))
 
-                       (method-names (mapcar
-                            #'car
-                            (coalton-impl/typechecker::ty-class-unqualified-methods
-                             (coalton-impl/typechecker::lookup-class env class-name))))
+                     (method-names (mapcar
+                                    #'car
+                                    (coalton-impl/typechecker::ty-class-unqualified-methods
+                                     (coalton-impl/typechecker::lookup-class env class-name))))
 
-                       (method-codegen-syms
-               (let ((table (make-hash-table)))
-                 (loop :for method-name :in method-names
-                       :do (setf (gethash method-name table)
-                                 (alexandria:format-symbol
-                                  package
-                                  "~A-~A"
-                                  instance-codegen-sym
-                                  method-name)))
-                 table))
+                     (method-codegen-syms
+                       (let ((table (make-hash-table)))
+                         (loop :for method-name :in method-names
+                               :do (setf (gethash method-name table)
+                                         (alexandria:format-symbol
+                                          package
+                                          "~A-~A"
+                                          instance-codegen-sym
+                                          method-name)))
+                         table))
 
-                       (instance
-                         (ty-class-instance
-                          :constraints context
-                          :predicate predicate
-                          :codegen-sym instance-codegen-sym
-                          :method-codegen-syms method-codegen-syms)))
+                     (instance
+                       (ty-class-instance
+                        :constraints context
+                        :predicate predicate
+                        :codegen-sym instance-codegen-sym
+                        :method-codegen-syms method-codegen-syms)))
 
-              (loop :for key :being :the :hash-keys :of method-codegen-syms
-              :for value :being :the :hash-values :of method-codegen-syms
-              :for codegen-sym := (coalton-impl/typechecker::ty-class-instance-codegen-sym instance)
-              :do (setf env (coalton-impl/typechecker::set-method-inline env key codegen-sym value)))
+                (loop :for key :being :the :hash-keys :of method-codegen-syms
+                      :for value :being :the :hash-values :of method-codegen-syms
+                      :for codegen-sym := (coalton-impl/typechecker::ty-class-instance-codegen-sym instance)
+                      :do (setf env (coalton-impl/typechecker::set-method-inline env key codegen-sym value)))
 
-                  (setf env (add-instance env class-name instance)))))
+                (when context
+                  (setf env (set-function
+                             env
+                             instance-codegen-sym
+                             (coalton-impl/typechecker::make-function-env-entry
+                              :name instance-codegen-sym
+                              :arity (length context)))))
+
+                (setf env (add-instance env class-name instance)))))
 
   env)
