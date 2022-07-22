b4a3ea4367730e85aba286e2cf9f23910612ceff
Alok Thapa <alokthapa@icloud.com>
2022-07-21 23:34:42 -0400

    fix double printing, add more tests

--- examples/coalton-json/tests/tests.lisp
+++ examples/coalton-json/tests/tests.lisp
@@ -1,5 +1,5 @@
 (fiasco:define-test-package #:coalton-json-tests
-  (:use #:cl)
+  (:use #:cl #:coalton-json)
   (:export #:run-coalton-json-tests))
 
 (cl:in-package #:coalton-json-tests)
@@ -12,37 +12,68 @@
 (defun is-type (ty x)
   (fiasco:is (typep x ty)))
 
+(fiasco:deftest test-json->string ()
+  (fiasco:is (equal "1.0" (coalton:coalton 
+                          (json->string (Json-Number 1d0)))))
+  (fiasco:is (equal "-2.0" (coalton:coalton 
+                          (json->string (Json-Number -2d0)))))
+  (fiasco:is (equal "0.00023" (coalton:coalton 
+                          (json->string (Json-Number 0.00023d0)))))
+  (fiasco:is (equal "909112334.454323" (coalton:coalton 
+                          (json->string (Json-Number 909112334.454323d0)))))
+  (fiasco:is (equal "\"helloworld\"" (coalton:coalton 
+                          (json->string (Json-String "helloworld")))))
+  (fiasco:is (equal "[1.0, 1.0, true, false, [], \"x\"]" 
+                    (coalton:coalton 
+                     (json->string 
+                      (parse-json "[1, 1.0, true, false, [], \"x\"]")))))
+  (fiasco:is (equal "{\"email\":\"john@email.com\", \"name\":\"John Smith\", \"id\":1.0}" 
+                    (coalton:coalton 
+                     (json->string     
+                      (Json-Object     
+                       (sm-insert      
+                        (sm-insert 
+                         (sm-insert 
+                          empty-sm
+                          "name" (Json-String "John Smith"))
+                         "email" (Json-String "john@email.com"))
+                                "id" (Json-Number 1d0)))))))
+  (fiasco:is (equal "false" (coalton:coalton 
+                          (json->string (Json-Boolean coalton:false)))))
+  (fiasco:is (equal "true" (coalton:coalton 
+                         (json->string (Json-Boolean coalton:true))))))
+
 (fiasco:deftest test-json ()
   (is-type 'coalton-json::json/json-null
-           (coalton-json:parse-json "null"))
+           (parse-json "null"))
   (is-type 'coalton-json::json/json-boolean
-           (coalton-json:parse-json "false"))
+           (parse-json "false"))
   (is-type 'coalton-json::json/json-boolean
-           (coalton-json:parse-json "true"))
+           (parse-json "true"))
   (is-type 'coalton-json::json/json-string
-           (coalton-json:parse-json "\"x\""))
+           (parse-json "\"x\""))
   (is-type 'coalton-json::json/json-number
-           (coalton-json:parse-json "1"))
+           (parse-json "1"))
   (is-type 'coalton-json::json/json-number
-           (coalton-json:parse-json "2.0"))
+           (parse-json "2.0"))
   (is-type 'coalton-json::json/json-number
-           (coalton-json:parse-json "2.0"))
+           (parse-json "2.0"))
   (is-type 'coalton-json::json/json-array
-           (coalton-json:parse-json "[]"))
+           (parse-json "[]"))
   (is-type 'coalton-json::json/json-array
-           (coalton-json:parse-json "[[]]"))
+           (parse-json "[[]]"))
   (is-type 'coalton-json::json/json-array
-           (coalton-json:parse-json "[1, 1.0, true, false, [], \"x\"]"))
+           (parse-json "[1, 1.0, true, false, [], \"x\"]"))
   (is-type 'coalton-json::json/json-array
-           (coalton-json::parse-json 
-            (coalton-json::json->string 
-             (coalton-json:parse-json "[1, 1.0, true, false, [], \"x\"]"))))
+           (parse-json 
+            (json->string 
+             (parse-json "[1, 1.0, true, false, [], \"x\"]"))))
   (is-type 'coalton-json::json/json-object
-           (coalton-json:parse-json "{}"))
+           (parse-json "{}"))
   (is-type 'coalton-json::json/json-object
-           (coalton-json:parse-json "{\"x\":0, \"y\":{\"z\":\"z\"}}"))
+           (parse-json "{\"x\":0, \"y\":{\"z\":\"z\"}}"))
   (is-type 'coalton-json::json/json-object
-           (coalton-json::parse-json 
-            (coalton-json::json->string
-             (coalton-json:parse-json "{\"x\":0, \"y\":{\"z\":\"z\"}}")))))
+           (parse-json 
+            (json->string
+             (parse-json "{\"x\":0, \"y\":{\"z\":\"z\"}}")))))
   
