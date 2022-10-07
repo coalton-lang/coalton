0a065d37a7c98e6d27b115a285a073106c1b8990
Cole Scott <cwscott@hrl.com>
2022-10-07 11:59:25 -0700

    Add basic Char functions

--- library/char.lisp
+++ library/char.lisp
@@ -42,7 +42,77 @@
           EQ
           (if (lisp Boolean (x y) (to-boolean (cl:char> x y)))
               GT
-              LT)))))
+              LT))))
+
+  (declare alpha? (Char -> Boolean))
+  (define (alpha? c)
+    "Is C an alphabetic character?"
+    (lisp Boolean (c)
+      (cl:alpha-char-p c)))
+
+  (declare ascii-alpha? (Char -> Boolean))
+  (define (ascii-alpha? c)
+    "Is C an ASCII alphabetic character?"
+    (lisp Boolean (c)
+      (cl:or
+       (cl:<= 65 (cl:char-code c) 90)
+       (cl:<= 97 (cl:char-code c) 122))))
+
+  (declare digit? (Char -> Boolean))
+  (define (digit? c)
+    "Is C a digit character?"
+    (lisp Boolean (c)
+      (to-boolean (cl:digit-char-p c))))
+
+  (declare ascii-digit? (Char -> Boolean))
+  (define (ascii-digit? c)
+    "Is C an ASCII digit character?"
+    (lisp Boolean (c)
+      (cl:<= 48 (cl:char-code c) 57)))
+
+  (declare ascii-alphanumeric? (Char -> Boolean))
+  (define (ascii-alphanumeric? c)
+    "Is C an ASCII alphanumeric character?"
+    (or (ascii-alpha? c)
+        (ascii-digit? c)))
+
+  (declare uppercase? (Char -> Boolean))
+  (define (uppercase? c)
+    "Is C an uppercase character?"
+    (lisp Boolean (c)
+      (cl:upper-case-p c)))
+
+  (declare ascii-uppercase? (Char -> Boolean))
+  (define (ascii-uppercase? c)
+    "Is C an ASCII uppercase character?"
+    (lisp Boolean (c)
+      (cl:or
+       (cl:<= 65 (cl:char-code c) 90))))
+
+  (declare lowercase? (Char -> Boolean))
+  (define (lowercase? c)
+    "Is C a lowercase character?"
+    (lisp Boolean (c)
+      (cl:lower-case-p c)))
+
+  (declare ascii-lowercase? (Char -> Boolean))
+  (define (ascii-lowercase? c)
+    "Is C an ASCII lowercase character?"
+    (lisp Boolean (c)
+      (cl:or
+       (cl:<= 97 (cl:char-code c) 122))))
+
+  (declare upcase (Char -> Char))
+  (define (upcase c)
+    "Returns the upcased version of C, returning C when there is none."
+    (lisp Char (c)
+      (cl:char-upcase c)))
+
+  (declare downcase (Char -> Char))
+  (define (downcase c)
+    "Returns the downcased version of C, returning C when there is none."
+    (lisp Char (c)
+      (cl:char-downcase c))))
 
 (define-sxhash-hasher Char)
 
