b11ccce1cfc5be10343b3b993609df3c839ef84e
Cole Scott <cwscott@hrl.com>
2023-01-13 15:53:08 -0800

    Remove trivia as a dependency and add custom pattern matching codegen

--- coalton.asd
+++ coalton.asd
@@ -20,7 +20,6 @@
                           #+sbcl (sb-ext:*block-compile-default* :specified))
                       (funcall compile)))
   :depends-on (#:alexandria
-               #:trivia
                #:fset
                #:float-features
                #:split-sequence
--- src/codegen/codegen-pattern.lisp
+++ src/codegen/codegen-pattern.lisp
@@ -11,50 +11,96 @@
 
 (in-package #:coalton-impl/codegen/codegen-pattern)
 
-(defgeneric codegen-pattern (pattern env)
-  (:method ((pattern ast:pattern-var) env)
+(defgeneric codegen-pattern (pattern expr env)
+  (:documentation "Codegen the match branch PATTERN on lisp value EXPR, returning (VALUES PREDICATE BINDINGS).")
+  (:method ((pattern ast:pattern-var) expr env)
     (declare (type tc:environment env)
              (ignore env))
-    (ast:pattern-var-id pattern))
-  (:method ((pattern ast:pattern-wildcard) env)
-    (declare (type coalton-impl/typechecker::environment env))
-    '_)
-  (:method ((pattern ast:pattern-literal) env)
-    (declare (type coalton-impl/typechecker::environment env))
-    (ast:pattern-literal-value pattern))
+    (values
+     `t
+     (list (list (ast:pattern-var-id pattern) expr))))
 
-  (:method ((pattern ast:pattern-constructor) env)
+  (:method ((pattern ast:pattern-wildcard) expr env)
+    (declare (type coalton-impl/typechecker::environment env))
+    (values `t nil))
+
+  (:method ((pattern ast:pattern-literal) expr env)
+    (declare (type coalton-impl/typechecker::environment env))
+    (values
+     `(eql (ast:pattern-literal-value pattern) ,expr)
+     nil))
+
+  (:method ((pattern ast:pattern-constructor) expr env)
     (declare (type coalton-impl/typechecker::environment env))
     (let* ((name (ast:pattern-constructor-name pattern))
            (entry (tc:lookup-constructor env name))
            (type (tc:lookup-type env (tc:constructor-entry-constructs entry)))
-           (class-name (tc:constructor-entry-classname entry))
            (package (symbol-package name)))
 
       (cond
+        ;; For enums we can just check symbol equality
         ((tc:type-entry-enum-repr type)
-         `',(tc:constructor-entry-compressed-repr entry))
+         (values
+          `(eql ',(tc:constructor-entry-compressed-repr entry) ,expr)
+          nil))
 
+        ;; For newtypes we can just codegen patterns for the inner type
         ((tc:type-entry-newtype type)
-         (progn
-           (unless (= 1 (length (ast:pattern-constructor-patterns pattern)))
-             (util:coalton-bug "Unexpected number of fields in newtype pattern.~%    Expected: 1~%    Received: ~A~%"
-                                        (length (ast:pattern-constructor-patterns pattern))))
-           (codegen-pattern (first (ast:pattern-constructor-patterns pattern)) env)))
+         (unless (= 1 (length (ast:pattern-constructor-patterns pattern)))
+           (util:coalton-bug "Unexpected number of fields in newtype pattern.~%    Expected: 1~%    Received: ~A~%"
+                             (length (ast:pattern-constructor-patterns pattern))))
+         (codegen-pattern (first (ast:pattern-constructor-patterns pattern)) expr env))
 
+        ;; Check cons directly
         ((eql (ast:pattern-constructor-name pattern) 'coalton:Cons)
-         `(cl:cons
-           ,(codegen-pattern (first (ast:pattern-constructor-patterns pattern)) env)
-           ,(codegen-pattern (second (ast:pattern-constructor-patterns pattern)) env)))
 
+         (multiple-value-bind (car-pred car-bindings)
+             (codegen-pattern (first (ast:pattern-constructor-patterns pattern))
+                              `(car ,expr)
+                              env)
+           (multiple-value-bind (cdr-pred cdr-bindings)
+               (codegen-pattern (second (ast:pattern-constructor-patterns pattern))
+                                `(cdr ,expr)
+                                env)
+             (values
+              `(and (cl:consp ,expr)
+                    ,car-pred
+                    ,cdr-pred)
+              (append car-bindings cdr-bindings)))))
+
+        ;; Check nil directly
         ((eql (ast:pattern-constructor-name pattern) 'coalton:Nil)
-         'cl:nil)
+         (values
+          `(null ,expr)
+          nil))
 
-
+        ;; Otherwise we need to bring out the big guns
         (t
-         (let ((fields (loop :for field :in (ast:pattern-constructor-patterns pattern)
-                             :for i :from 0
-                             :for subpattern := (codegen-pattern field env)
-                             :for accessor := (alexandria:format-symbol package "_~D" i)
-                             :collect `(,accessor ,subpattern))))
-           `(cl:structure ,class-name ,@fields)))))))
+         (let* ((ctor-name (ast:pattern-constructor-name pattern))
+                (ctor (tc:lookup-constructor env ctor-name))
+                (lisp-type-string (format nil "~A/~A" (tc:constructor-entry-constructs ctor) ctor-name)))
+           (multiple-value-bind (preds bindings)
+               (loop :with preds := nil
+                     :with bindings := nil
+
+                     :for field :in (ast:pattern-constructor-patterns pattern)
+                     :for i :from 0
+
+                     :for accessor := (alexandria:format-symbol package "~A-_~D" lisp-type-string i)
+
+                     :do (multiple-value-bind (field-pred field-bindings)
+                             (codegen-pattern
+                              field
+                              `(,accessor ,expr)
+                              env)
+                           (push field-pred preds)
+                           (push field-bindings bindings))
+                     :finally (return (values (nreverse preds)
+                                              (nreverse bindings))))
+
+             ()
+             (values
+              `(and
+                (typep ,expr ',(alexandria:format-symbol package "~A" lisp-type-string))
+                ,@preds)
+              (reduce #'append bindings)))))))))
