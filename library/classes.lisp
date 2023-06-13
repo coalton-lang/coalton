79c039e17dc0921754078bec1634d496a073c229
Pietro Cerutti <gahr@gahr.ch>
2023-06-13 09:16:42 +0000

    Fix description of INTO

--- library/classes.lisp
+++ library/classes.lisp
@@ -246,7 +246,7 @@
   ;;
 
   (define-class (Into :a :b)
-    "INTO imples *every* element of :a can be represented by an element of :b. This conversion might not be injective (i.e., there may be elements in :a that don't correspond to any in :b)."
+    "INTO imples *every* element of :a can be represented by an element of :b. This conversion might not be bijective (i.e., there may be elements in :b that don't correspond to any in :a)."
     (into (:a -> :b)))
 
   (define-class ((Into :a :b) (Into :b :a) => Iso :a :b)
