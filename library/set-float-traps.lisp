ecabb7bf659a1bdcc777804a984ea98e57b474c8
Robert Smith <rssmith@hrl.com>
2024-03-22 13:47:18 -0700

    qualify EVAL-WHEN in a file w/no IN-PACKAGE

--- library/set-float-traps.lisp
+++ library/set-float-traps.lisp
@@ -1,3 +1,3 @@
-(eval-when (:compile-toplevel :load-toplevel :execute)
+(cl:eval-when (:compile-toplevel :load-toplevel :execute)
   #+ccl (ccl:set-fpu-mode :overflow nil :underflow nil :division-by-zero nil :invalid nil :inexact nil)
   #+sbcl (sb-int:set-floating-point-modes :traps nil))
