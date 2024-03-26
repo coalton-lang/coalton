139919b23390a617ea91cc2679fb5a937291eaf0
Walton, Izaak (VEN) <iwalton@hrl.com>
2024-03-25 20:51:42 -0700

    Update thih-coalton, thih-coalton/tests

--- examples/thih/tests/thih-coalton-tests.lisp
+++ examples/thih/tests/thih-coalton-tests.lisp
@@ -1,34 +1,37 @@
-(cl:in-package #:thih-coalton-tests)
+(in-package #:thih-coalton/tests)
 
-(cl:defparameter *initial-env* (coalton-prelude:from-some "Failed to init classenv"
-                                                         (compose
-                                                          addPreludeClasses
-                                                          exampleInsts
-                                                          initialEnv)))
+(named-readtables:in-readtable coalton:coalton)
 
-(deftest test-type-inference ()
-  (is (thih-coalton::assumption-list-equal (tiProgram
-                                            *initial-env*
-                                            Nil
-                                            (Program
-                                             (make-list
-                                              (BindGroup
-                                               Nil
-                                               (make-list
-                                                (make-list
-                                                 (Impl
-                                                  (Id "f")
-                                                  (make-list
-                                                   (Alt
-                                                    (make-list
-                                                     (PVar (Id "x")))
-                                                    (Var (Id "x")))))))))))
+(coalton-toplevel
+  (define *initial-env* (from-some "Failed to init classenv"
+                                   (th:compose
+                                    th:addPreludeClasses
+                                    th:exampleInsts
+                                    th:initialEnv))))
+
+(define-test test-thih-type-inference ()
+  (is (th::assumption-list-equal (th:tiProgram
+                                  *initial-env*
+                                  Nil
+                                  (th:Program
+                                   (make-list
+                                    (th:BindGroup
+                                     Nil
+                                     (make-list
+                                      (make-list
+                                       (th:Impl
+                                        (th:Id "f")
+                                        (make-list
+                                         (th:Alt
+                                          (make-list
+                                           (th:PVar (th:Id "x")))
+                                          (th:Var (th:Id "x")))))))))))
               
-                                           (make-list
-                                            (Assump
-                                             (Id "f")
-                                             (Forall
-                                              (make-list Star)
-                                              (Qual Nil
-                                                    (mkFn (TGen 0)
-                                                          (TGen 0)))))))))
+                                 (make-list
+                                  (th:Assump
+                                   (th:Id "f")
+                                   (th:Forall
+                                    (make-list th:Star)
+                                    (th:Qual Nil
+                                             (th:mkFn (th:TGen 0)
+                                                      (th:TGen 0)))))))))
--- examples/thih/thih-coalton.asd
+++ examples/thih/thih-coalton.asd
@@ -5,14 +5,16 @@
   :depends-on (#:coalton)
   :pathname "src/"
   :serial t
+  :in-order-to ((test-op (test-op "thih-coalton/tests")))
   :components ((:file "package")
                (:file "thih")))
 
 (asdf:defsystem #:thih-coalton/tests
   :description "Tests for THIH-COALTON"
-  :depends-on (#:thih-coalton #:fiasco)
+  :depends-on (#:thih-coalton #:fiasco #:coalton/testing)
   :pathname "tests/"
   :serial t
   :components ((:file "package")
-               (:file "thih-coalton-tests")
-               (:file "utilities")))
+               (:file "thih-coalton-tests"))
+  :perform (test-op (o s)
+                    (symbol-call '#:thih-coalton/tests '#:run-tests)))
