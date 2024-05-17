7b4e05f9aaec87e10fdec5e67cb3fd164e430cb1
Jesse Bouwman <jesse.bouwman@proton.me>
2024-05-17 11:24:27 -0700

    An ASDF component type that can process files with .coalton extension.

---
+++ coalton-asdf.lisp
@@ -0,0 +1,28 @@
+;; ASDF Coalton component type
+;;
+;; Add an asdf component that supports compiling files with a .coalton
+;; extension.
+;;
+;; To use, add a :defsystem-depends-on clause to your system
+;; definition, and refer to files using component type :coalton-file,
+;; like:
+;;
+;;     (asdf:defsystem #:application
+;;       :defsystem-depends-on (#:coalton-asdf)
+;;       :depends-on (#:coalton)
+;;       :pathname "src/"
+;;       :serial t
+;;       :components ((:coalton-file "types")
+;;                    (:coalton-file "main")))
+;;
+;; This class is defined in the ASDF package, so that the keyword :coalton-file works.
+;;
+;; see: https://github.com/fare/asdf/blob/master/doc/best_practices.md#using-asdf-extensions
+
+(in-package :asdf)
+
+(defclass coalton-file (cl-source-file)
+  ((type :initform "coalton")))
+
+(defmethod source-file-type ((c coalton-file) (s module))
+  "coalton")
