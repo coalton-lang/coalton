26233a665c6d81b90be653cc7c5d21fd794706bf
Jesse Bouwman <jlbouwman@hrl.com>
2024-06-17 09:01:19 -0700

    Extend package form
    - support import, import-as, export, shadow
    - example in examples/small-coalton-programs/src/classical.coal
    - documentation in doc/package.md

---
+++ coalton-asdf.lisp
@@ -0,0 +1,16 @@
+;;; Coalton ASDF extensions
+;;;
+;;; This class is defined in the ASDF package, so that the keyword :coalton-file works.
+;;;
+;;; see: https://github.com/fare/asdf/blob/master/doc/best_practices.md#using-asdf-extensions
+
+(in-package :asdf)
+
+(defclass coalton-file (cl-source-file)
+  ((type :initform "coal")))
+
+(defmethod perform ((o compile-op) (c coalton-file))
+  (let ((coal-file (first (input-files o c)))
+        (fasl-file (first (output-files o c))))
+    (coalton-impl/entry:compile-coalton coal-file
+                                        :output-file fasl-file)))
