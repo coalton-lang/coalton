f56d4c72a95f91e25d3fba156c495e88d952f036
Jesse Bouwman <jlbouwman@hrl.com>
2024-10-02 11:51:47 -0700

    Add documentation to methods of the Bits class

--- library/bits.lisp
+++ library/bits.lisp
@@ -27,11 +27,11 @@
 (coalton-toplevel
   (define-class (Num :int => Bits :int)
     "Operations on the bits of twos-complement integers"
-    (and (:int -> :int -> :int))
-    (or (:int -> :int -> :int))
-    (xor (:int -> :int -> :int))
-    (not (:int -> :int))
-    (shift (Integer -> :int -> :int))))
+    (and "The bitwise logical and of two integers" (:int -> :int -> :int))
+    (or "The bitwise logical or of two integers" (:int -> :int -> :int))
+    (xor "The bitwise logical exclusive or of two integers" (:int -> :int -> :int))
+    (not "The bitwise logical not of two integers" (:int -> :int))
+    (shift "The arithmetic left-shift of an integer by an integer number of bits" (Integer -> :int -> :int))))
 
 #+sb-package-locks
 (sb-ext:lock-package "COALTON-LIBRARY/BITS")
