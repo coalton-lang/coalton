e80eeb160fe2589c8f279b0847b21cac5677bfaf
garlic0x1 <garlic1@protonmail.com>
2024-12-26 13:00:41 -0600

    more tests and docstrings

--- library/threads.lisp
+++ library/threads.lisp
@@ -30,7 +30,7 @@
    ;; Condition Variables
    #:ConditionVariable
    #:make-cv
-   #:wait-cv
+   #:await-cv
    #:notify-cv
    #:broadcast-cv
    ;; Semaphores
@@ -75,42 +75,51 @@
       (lisp Boolean (a b) (to-boolean (cl:equal a b)))))
 
   (declare make-thread ((Unit -> Unit) -> Thread))
-  (define (make-thread f)
-    (lisp Thread (f)
+  (define (make-thread thunk)
+    "Creates and returns a thread, which will call the function
+`thunk' with no arguments: when `thunk' returns, the thread terminates."
+    (lisp Thread (thunk)
       (bt2:make-thread
-       (cl:lambda () (call-coalton-function f Unit)))))
+       (cl:lambda () (call-coalton-function thunk Unit)))))
 
   (declare current-thread (Unit -> Thread))
   (define (current-thread)
+    "Returns the thread object representing the calling thread."
     (lisp Thread ()
       (bt2:current-thread)))
 
   (declare all-threads (Unit -> (List Thread)))
   (define (all-threads)
+    "Returns a fresh list of all running threads."
     (lisp (List Thread) ()
       (bt2:all-threads)))
 
   (declare join (Thread -> Unit))
   (define (join thread)
+    "Wait until `thread' terminates, or if it has already terminated, return immediately."
     (lisp Unit (thread)
       (bt2:join-thread thread)
       Unit))
 
-  (declare interrupt (Thread -> (Unit -> Unit) -> Unit))
-  (define (interrupt thread f)
-    (lisp Unit (thread f)
+  (declare interrupt (Thread -> (Unit -> Unit) -> Thread))
+  (define (interrupt thread thunk)
+    "Interrupt thread and call `thunk' within its dynamic context,
+then continue with the interrupted path of execution."
+    (lisp Thread (thread thunk)
       (bt2:interrupt-thread
        thread
-       (cl:lambda () (call-coalton-function f Unit)))))
+       (cl:lambda () (call-coalton-function thunk Unit)))))
 
   (declare destroy (Thread -> (Result LispCondition Thread)))
   (define (destroy thread)
+    "Terminates the thread `thread'."
     (lisp (Result LispCondition Thread) (thread)
-      (cl:handler-case (Ok (bt2:join-thread thread))
+      (cl:handler-case (Ok (bt2:destroy-thread thread))
         (cl:error (c) (Err c)))))
 
   (declare alive? (Thread -> Boolean))
   (define (alive? thread)
+    "Returns True if `thread' has not finished or `destroy' has not been called on it."
     (lisp Boolean (thread)
       (bt2:thread-alive-p thread))))
 
@@ -141,43 +150,65 @@
 
   (declare make-lock (Unit -> Lock))
   (define (make-lock)
+    "Creates a non-recursive lock."
     (lisp Lock ()
       (bt2:make-lock)))
 
   (declare acquire-lock (Lock -> Boolean))
   (define (acquire-lock lock)
+    "Acquire `lock' for the calling thread."
     (lisp Boolean (lock)
       (bt2:acquire-lock lock)))
 
   (declare acquire-lock-no-wait (Lock -> Boolean))
   (define (acquire-lock-no-wait lock)
+    "Acquire `lock' for the calling thread.
+Returns Boolean immediately, True if `lock' was acquired, False otherwise."
     (lisp Boolean (lock)
       (bt2:acquire-lock lock :wait nil)))
 
-  (declare release-lock (Lock -> Lock))
+  (declare release-lock (Lock -> (Result LispCondition Lock)))
   (define (release-lock lock)
-    (lisp Lock (lock)
-      (bt2:release-lock lock)))
+    "Release `lock'. It is an error to call this unless
+the lock has previously been acquired (and not released) by the same
+thread. If other threads are waiting for the lock, the
+`acquire-lock' call in one of them will now be able to continue.
+
+Returns the lock."
+    (lisp (Result LispCondition Lock) (lock)
+      (cl:handler-case (Ok (bt2:release-lock lock))
+        (cl:error (c) (Err c)))))
 
   (declare make-recursive-lock (Unit -> RecursiveLock))
   (define (make-recursive-lock)
+    "Creates a recursive lock."
     (lisp RecursiveLock ()
       (bt2:make-recursive-lock)))
 
   (declare acquire-recursive-lock (RecursiveLock -> Boolean))
   (define (acquire-recursive-lock lock)
+    "Acquire `lock' for the calling thread."
     (lisp Boolean (lock)
       (bt2:acquire-recursive-lock lock)))
 
   (declare acquire-recursive-lock-no-wait (Lock -> Boolean))
   (define (acquire-recursive-lock-no-wait lock)
+    "Acquire `lock' for the calling thread.
+Returns Boolean immediately, True if `lock' was acquired, False otherwise."
     (lisp Boolean (lock)
       (bt2:acquire-recursive-lock lock :wait nil)))
 
-  (declare release-recursive-lock (RecursiveLock -> RecursiveLock))
+  (declare release-recursive-lock (RecursiveLock -> (Result LispCondition RecursiveLock)))
   (define (release-recursive-lock lock)
-    (lisp RecursiveLock (lock)
-      (bt2:release-recursive-lock lock))))
+    "Release `lock'. It is an error to call this unless
+the lock has previously been acquired (and not released) by the same
+thread. If other threads are waiting for the lock, the
+`acquire-lock' call in one of them will now be able to continue.
+
+Returns the lock."
+    (lisp (Result LispCondition RecursiveLock) (lock)
+      (cl:handler-case (Ok (bt2:release-recursive-lock lock))
+        (cl:error (c) (Err c))))))
 
 ;;---------------------;;
 ;; Condition Variables ;;
@@ -189,23 +220,31 @@
 
   (declare make-cv (Unit -> ConditionVariable))
   (define (make-cv)
+    "Creates a condition variable."
     (lisp ConditionVariable ()
       (bt2:make-condition-variable)))
 
-  (declare wait-cv (ConditionVariable -> Lock -> Unit))
-  (define (wait-cv cv lock)
+  (declare await-cv (ConditionVariable -> Lock -> Unit))
+  (define (await-cv cv lock)
+    "Atomically release `lock' and enqueue the calling thread waiting for `cv'.
+The thread will resume when another thread has notified it using `notify-cv';
+it may also resume if interrupted by some external event or in other
+implementation-dependent circumstances: the caller must always test on waking
+that there is threading to be done, instead of assuming that it can go ahead."
     (lisp Unit (cv lock)
       (bt2:condition-wait cv lock)
       Unit))
 
   (declare notify-cv (ConditionVariable -> Unit))
   (define (notify-cv cv)
+    "Notify one of the threads waiting for `cv'."
     (lisp Unit (cv)
       (bt2:condition-notify cv)
       Unit))
 
   (declare broadcast-cv (ConditionVariable -> Unit))
   (define (broadcast-cv cv)
+    "Notify all of the threads waiting for `cv'."
     (lisp Unit (cv)
       (bt2:condition-broadcast cv)
       Unit)))
@@ -220,17 +259,22 @@
 
   (declare make-semaphore (Unit -> Semaphore))
   (define (make-semaphore)
+    "Creates a semaphore with initial count 0."
     (lisp Semaphore ()
       (bt2:make-semaphore)))
 
   (declare signal-semaphore (Semaphore -> UFix -> Unit))
   (define (signal-semaphore sem count)
+    "Increment `sem' by `count'.
+If there are threads awaiting this semaphore, then `count' of them are woken up."
     (lisp Unit (sem count)
       (bt2:signal-semaphore sem :count count)
       Unit))
 
   (declare await-semaphore (Semaphore -> Unit))
   (define (await-semaphore sem)
+    "Decrement the count of `sem' by 1 if the count is larger than zero.
+If the count is zero, blocks until `sem' can be decremented."
     (lisp Unit (sem)
       (bt2:wait-on-semaphore sem)
       Unit)))
@@ -245,27 +289,31 @@
 
   (declare make-atomic (#+32-bit U32 #+64-bit U64 -> AtomicInteger))
   (define (make-atomic value)
+    "Creates an `AtomicInteger' with initial value `value'."
     (lisp AtomicInteger (value)
       (bt2:make-atomic-integer :value value)))
 
   (declare atomic-cmp-and-swap (AtomicInteger -> #+32-bit U32 #+64-bit U64 -> #+32-bit U32 #+64-bit U64 -> Boolean))
   (define (atomic-cmp-and-swap atomic old new)
+    "If the current value of `atomic' is equal to `old', replace it with `new'.
+Returns True if the replacement was successful, otherwise False."
     (lisp Boolean (atomic old new)
       (bt2:atomic-integer-compare-and-swap atomic old new)))
 
-  (declare decf-atomic (AtomicInteger -> Unit))
-  (define (decf-atomic atomic)
-    (lisp Unit (atomic)
-      (bt2:atomic-integer-decf atomic)
-      Unit))
+  (declare decf-atomic (AtomicInteger -> #+32-bit U32 #+64-bit U64 -> #+32-bit U32 #+64-bit U64))
+  (define (decf-atomic atomic delta)
+    "Decrements the value of `atomic' by `delta'."
+    (lisp #+32-bit U32 #+64-bit U64 (atomic delta)
+      (bt2:atomic-integer-decf atomic delta)))
 
-  (declare incf-atomic (AtomicInteger -> Unit))
-  (define (incf-atomic atomic)
-    (lisp Unit (atomic)
-      (bt2:atomic-integer-incf atomic)
-      Unit))
+  (declare incf-atomic (AtomicInteger -> #+32-bit U32 #+64-bit U64 -> #+32-bit U32 #+64-bit U64))
+  (define (incf-atomic atomic delta)
+    "Increments the value of `atomic' by `delta'."
+    (lisp #+32-bit U32 #+64-bit U64 (atomic delta)
+      (bt2:atomic-integer-incf atomic delta)))
 
   (declare atomic-value (AtomicInteger -> #+32-bit U32 #+64-bit U64))
   (define (atomic-value atomic)
+    "Returns the current value of `atomic'."
     (lisp #+32-bit U32 #+64-bit U64 (atomic)
       (bt2:atomic-integer-value atomic))))
--- tests/thread-tests.lisp
+++ tests/thread-tests.lisp
@@ -4,7 +4,9 @@
   (define (sleep n)
     (lisp Unit (n) (cl:sleep n) Unit)))
 
-;; Threads
+;;---------;;
+;; Threads ;;
+;;---------;;
 
 (define-test thread-spawn-and-join ()
   (let ((thread (threads:spawn (sleep 1))))
@@ -19,12 +21,14 @@
 
 (define-test thread-all-threads-count ()
   (let ((count (length (threads:all-threads)))
-        (thread (threads:spawn (sleep 60))))
+        (thread (threads:spawn (sleep 1))))
     (is (== (length (threads:all-threads)) (1+ count)))
     (threads:destroy thread)
     (is (== (length (threads:all-threads)) count))))
 
-;; Locks
+;;-------;;
+;; Locks ;;
+;;-------;;
 
 (define-test lock-acquire-release ()
   (let ((lock (threads:make-lock)))
@@ -41,9 +45,9 @@
     (sleep 1)
     (is (not (threads:acquire-lock-no-wait lock)))))
 
-;; TODO Recursive Locks
-
-;; Semaphores
+;;------------;;
+;; Semaphores ;;
+;;------------;;
 
 (define-test semaphore-signal-await ()
   (let ((sem (threads:make-semaphore)))
@@ -60,26 +64,48 @@
     (threads:spawn
       (sleep 1)
       (threads:signal-semaphore sem 4))
-    ;; idk dotimes
     (for x in (range 1 5)
       (threads:spawn 
         (threads:await-semaphore sem)
-        (threads:incf-atomic count)))
+        (threads:incf-atomic count 1)))
     (sleep 1)
     (is (== 4 (threads:atomic-value count)))
     (threads:signal-semaphore sem 1)
     (sleep 1)
     (is (== 5 (threads:atomic-value count)))))
 
-;; TODO Condition Variables
+;;---------------------;;
+;; Condition Variables ;;
+;;---------------------;;
 
-;; Atomics
+(define-test condition-variable-concurrency ()
+  (let ((atomic (threads:make-atomic 0))
+        (cv (threads:make-cv))
+        (lock (threads:make-lock)))
+    (iter:for-each! 
+     (fn (_)
+       (threads:spawn
+         (threads:with-lock-held (lock)
+           (threads:await-cv cv lock))
+         (threads:incf-atomic atomic 1))
+       Unit)
+     (iter:repeat-for 0 100))
+    (sleep 1)
+    (iter:for-each! threads:notify-cv (iter:repeat-for cv 25))
+    (sleep 1)
+    (is (== 25 (threads:atomic-value atomic)))
+    (threads:broadcast-cv cv)
+    (sleep 1)
+    (is (== 100 (threads:atomic-value atomic)))))
+
+;;---------;;
+;; Atomics ;;
+;;---------;;
 
 (define-test atomic-incf-decf ()
   (let ((atomic (threads:make-atomic 3)))
-    (threads:incf-atomic 10)
-    (threads:decf-atomic 4)
-    (is (== 9 (threads:atomic-value atomic)))))
+    (is (== 13 (threads:incf-atomic atomic 10)))
+    (is (== 9 (threads:decf-atomic atomic 4)))))
 
 (define-test atomic-cmp-and-swap ()
   (let ((atomic (threads:make-atomic 4)))
@@ -91,10 +117,10 @@
   (let ((atomic (threads:make-atomic 4000))
         (incer (threads:spawn
                  (for x in (range 1 1000)
-                   (threads:incf-atomic atomic))))
+                   (threads:incf-atomic atomic 1))))
         (decer (threads:spawn
                  (for x in (range 1 1000)
-                   (threads:decf-atomic atomic)))))
+                   (threads:decf-atomic atomic 1)))))
     (threads:join incer)
     (threads:join decer)
     (is (== 4000 (threads:atomic-value atomic)))))
