f262d169b303798c0a0fd13818f943999f22a7bb
Yarin Heffes <yarin.heffes@gmail.com>
2024-12-30 13:25:33 -0800

    Added more define-type-alias tests

--- tests/type-alias-tests.lisp
+++ tests/type-alias-tests.lisp
@@ -82,10 +82,31 @@
   (check-coalton-types
    "(define-type-alias (Collapse :a :b :c :d) (:d -> :c -> :b -> :a))"))
 
+(deftest test-complex-type-alias-definition ()
+
+  (check-coalton-types
+   "(define-type (T :a :b :c) (T (:a -> :b :c)))
+    (define-struct S (x Integer))
+    (define-type-alias A1 (Tuple Integer))
+    (define-type-alias A2 (T S A1 Integer))")
+
+  (check-coalton-types
+   "(define-type-alias (A :a :b) (Tuple :a (Tuple :b :b)))
+    (declare f ((A Integer Integer) -> Integer))
+    (define (f (Tuple a (Tuple b c))) (+ a (+ b c)))"))
+
 (deftest test-parametric-type-alias-the ()
   (check-coalton-types
    "(define-type-alias Index UFix)
     (define-type-alias (Collection :a) (List :a))
+
+    (define l (the (Collection Index) (make-list 1 2 3 4)))"
+
+   '("l" . "(List UFix)"))
+
+  (check-coalton-types
+   "(define-type-alias Index UFix)
+    (define-type-alias Collection List)
 
     (define l (the (Collection Index) (make-list 1 2 3 4)))"
 
@@ -99,6 +120,15 @@
     (define f 1+)"
 
    '("f" . "(Integer -> Integer)"))
+
+  
+  (check-coalton-types
+   "(define-type-alias (FoldFunc :a :b) (:a -> :b -> :a))
+
+    (declare f (FoldFunc (List Integer) Integer))
+    (define (f xs x) (Cons x xs))"
+
+   '("f" . "((List Integer) -> Integer -> (List Integer))"))
 
   (check-coalton-types
    "(define-type-alias (UnaryOperator :a) (:a -> :a))
