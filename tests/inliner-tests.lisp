2ad7d0efb7ab8f322a5eee87eef7a3c53f3892f3
Yarin Heffes <yarin.heffes@gmail.com>
2025-01-16 22:54:07 -0500

    added test for rename-bound-variables with inlining

--- tests/inliner-tests.lisp
+++ tests/inliner-tests.lisp
@@ -6,3 +6,17 @@
   ;; See gh #1202
   (is (== 1 (1+
              ((fn (x) (return x)) 0)))))
+
+(in-package #:coalton-tests)
+
+;; See gh #1293
+(deftest test-inliner-rename-bound-variables ()
+  (check-coalton-types
+   "(declare f (Integer -> Integer))
+    (define (f n)
+      (when (== n 0)
+        (return 0))
+      (when (== n 1)
+        (return 1))
+      (+ (f (- n 1))
+         (f (- n 2))))"))
