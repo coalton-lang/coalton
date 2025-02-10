b8a05e79f98f55fb95572e4c45e9796b9f4fb193
Yarin Heffes <yarin.heffes@gmail.com>
2025-02-10 12:02:39 -0800

    add inlining for boolean functions

--- library/builtin.lisp
+++ library/builtin.lisp
@@ -26,18 +26,24 @@
      (cl:error ,datum ,@arguments)))
 
 (coalton-toplevel
+  (inline)
   (define (undefined _)
     "A function which can be used in place of any value, throwing an error at runtime."
     (error "Undefined"))
 
-  (define not
+  (inline)
+  (declare not (Boolean -> Boolean))
+  (define (not x)
     "Synonym for `boolean-not`."
-    boolean-not)
+    (boolean-not x))
 
-  (define xor
+  (inline)
+  (declare xor (Boolean -> Boolean -> Boolean))
+  (define (xor x y)
     "Synonym for `boolean-xor`."
-    boolean-xor)
+    (boolean-xor x y))
 
+  (inline)
   (declare boolean-not (Boolean -> Boolean))
   (define (boolean-not x)
     "The logical negation of `x`. Is `x` false?"
@@ -45,6 +51,7 @@
       ((True) False)
       ((False) True)))
 
+  (inline)
   (declare boolean-or (Boolean -> Boolean -> Boolean))
   (define (boolean-or x y)
     "Is either `x` or `y` true? Note that this is a *function* which means both `x` and `y` will be evaluated. Use the `or` macro for short-circuiting behavior."
@@ -52,6 +59,7 @@
       ((True) True)
       ((False) y)))
 
+  (inline)
   (declare boolean-and (Boolean -> Boolean -> Boolean))
   (define (boolean-and x y)
     "Are both `x` and `y` true? Note that this is a *function* which means both `x` and `y` will be evaluated. Use the `and` macro for short-circuiting behavior."
@@ -59,6 +67,7 @@
       ((True) y)
       ((False) False)))
 
+  (inline)
   (declare boolean-xor (Boolean -> Boolean -> Boolean))
   (define (boolean-xor x y)
     "Are `x` or `y` true, but not both?"
