a9a0bb5926cfb0fb4ca9e1a0c81e47e523baf397
Robert Smith <robert@stylewarning.com>
2025-03-26 14:40:31 -0400

    move specializer to its own file

--- src/codegen/optimizer.lisp
+++ src/codegen/optimizer.lisp
@@ -50,6 +50,9 @@
    #:coalton-impl/codegen/inliner
    #:inline-applications
    #:inline-methods)
+  (:import-from
+   #:coalton-impl/codegen/specializer
+   #:apply-specializations)
   (:local-nicknames
    (#:settings #:coalton-impl/settings)
    (#:util #:coalton-impl/util)
@@ -483,63 +486,6 @@
      node
      (list
       (action (:after node-field) #'handle-static-superclass)))))
-
-(defun apply-specializations (node env)
-  (declare (type node node)
-           (type tc:environment env)
-           (values node &optional))
-
-  (when settings:*coalton-disable-specialization*
-    (return-from apply-specializations node))
-
-  (labels ((apply-specialization (node)
-             (let ((rator-name (node-rator-name node)))
-               (unless rator-name
-                 (return-from apply-specialization))
-
-               (let ((from-ty (tc:lookup-value-type env rator-name :no-error t)))
-                 (unless from-ty
-                   (return-from apply-specialization))
-
-                 (let* ((from-ty (tc:fresh-inst from-ty))
-
-                        (preds (tc:qualified-ty-predicates from-ty))
-
-                        (num-preds (length preds))
-
-                        (rator-type
-                          (tc:make-function-type*
-                           (subseq (tc:function-type-arguments (node-rator-type node)) num-preds)
-                           (tc:function-return-type (node-rator-type node))))
-
-                        (specialization (tc:lookup-specialization-by-type env rator-name rator-type :no-error t)))
-                   (unless specialization
-                     (return-from apply-specialization))
-
-                   (unless (>= (length (node-rands node)) num-preds)
-                     (util:coalton-bug "Expected function ~A to have at least ~A args when applying specialization." rator-name (length preds)))
-
-                   (cond
-                     ((= num-preds (length (node-rands node)))
-                      (make-node-variable
-                       :type rator-type
-                       :value (tc:specialization-entry-to specialization)))
-
-                     ((< num-preds (length (node-rands node)))
-                      (make-node-application
-                       :type (node-type node)
-                       :rator (make-node-variable
-                               :type rator-type
-                               :value (tc:specialization-entry-to specialization))
-                       :rands (subseq (node-rands node) num-preds)))
-
-                     (t
-                      (util:coalton-bug "Invalid specialization ~A~%" specialization))))))))
-    (traverse
-     node
-     (list
-      (action (:after node-application) #'apply-specialization)
-      (action (:after node-direct-application) #'apply-specialization)))))
 
 (defun match-dynamic-extent-lift (node env)
   "Stack allocates uncaptured ADTs constructed in the head of a match expression"
