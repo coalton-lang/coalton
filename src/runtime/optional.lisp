841033ab4e4c071f55d449fe5a910459fd3cd005
Robert Smith <robert@stylewarning.com>
2025-07-02 20:26:10 -0400

    make cl-none and friends eq-constant

--- src/runtime/optional.lisp
+++ src/runtime/optional.lisp
@@ -27,57 +27,95 @@
 (in-package #:coalton-impl/runtime/optional)
 
 (eval-when (:compile-toplevel :load-toplevel :execute)
-  (declaim (inline make-cl-optional))
-  (defstruct (cl-optional (:constructor make-cl-optional (depth)))
+  (defstruct (cl-optional (:constructor %make-cl-optional (depth))
+                          (:predicate nil)
+                          (:copier nil)
+                          (:conc-name nil))
     "A CL-OPTIONAL represents of DEPTH N represents the Coalton
 value (Some^N None)."
     (depth 0 :type (unsigned-byte 16) :read-only t))
 
-  ;; SBCL generates the load form by default, but CCL
-  ;; does not, so we generate it here regardless.
-  (defmethod make-load-form ((obj cl-optional) &optional env)
-    (make-load-form-saving-slots obj :environment env)))
+  #+sbcl
+  (cl:declaim (sb-ext:freeze-type cl-optional))
 
-#+sbcl
-(cl:declaim (sb-ext:freeze-type cl-optional))
+  ;; Below is some pretty cursed code.
+  ;;
+  ;; The goal here is to cache some CL-OPTIONAL instances for small
+  ;; depths. We want to make sure of the following:
+  ;;
+  ;;     - We can compare None, which is (%make-cl-optional 0), with
+  ;;       CL:EQ.
+  ;;
+  ;;     - All of the constants should survive as objects in file
+  ;;       compilation, which means MAKE-LOAD-FORM needs to work.
+  ;;
+  ;;     - It must be portable, at least to SBCL and CCL.
+  ;;
+  ;; There must be a better way to do the below; I'm just not sure
+  ;; what it is.
+  (defconstant cl-optional-cache-size 4)
+  
+  (unless (get 'cl-optional ':cache)
+    (setf (get 'cl-optional ':cache)
+          (loop :for i :below cl-optional-cache-size
+                :collect (%make-cl-optional i))))
+  
+  (defmethod make-load-form ((obj cl-optional) &optional env)
+    (declare (ignore env))
+    (if (< (depth obj) cl-optional-cache-size)
+        `(nth ,(depth obj) (get 'cl-optional ':cache))
+        `(%make-cl-optional ,(depth obj))))
+
+  ;; Note: This is a throwaway macro used just once for its
+  ;; compile-time nature. We could probably do it just as well with
+  ;; macrolet.
+  (defmacro define-cached-constructor (name)
+    (let ((cached-optionals (loop :for i :below cl-optional-cache-size
+                                  :collect (intern (format nil "%NONE~D" i)))))
+      `(progn
+         ;; First, define global constants which refer to each cached
+         ;; value. They will all be named %NONE<n>.
+         (eval-when (:compile-toplevel :load-toplevel :execute)
+           ,@(loop :for i :below cl-optional-cache-size
+                   :for s :in cached-optionals
+                   :collect `(defconstant ,s
+                               (if (boundp ',s)
+                                   (symbol-value ',s)
+                                   (nth ,i (get 'cl-optional ':cache))))))
+
+         ;; Second, define an actual constructor function that can
+         ;; efficiently produce our cached results, including when the
+         ;; argument is known.
+         (declaim (ftype (function ((unsigned-byte 16)) cl-optional) ,name)
+                  (inline make-cl-optional))
+         (defun ,name (depth)
+           (case depth
+             ,@(loop :for i :below cl-optional-cache-size
+                     :for s :in cached-optionals
+                     :collect `((,i) ,s))
+             (otherwise (%make-cl-optional depth))))))))
+
+(define-cached-constructor make-cl-optional)
+
+(define-symbol-macro cl-none %none0)
 
 (defmethod print-object ((opt cl-optional) stream)
-  (let ((depth (cl-optional-depth opt)))
+  (let ((depth (depth opt)))
     (format stream "#.")
     (loop :repeat depth :do (format stream "(~S " 'coalton:Some))
     (format stream "~S" 'coalton:None)
     (loop :repeat depth :do (format stream ")"))))
 
-(alexandria:define-constant +cl-optional-cache-size+ 8)
-
-(declaim (type (simple-vector 8) cl-optional-cache))
-(alexandria:define-constant cl-optional-cache
-    (make-array +cl-optional-cache-size+
-                :initial-contents
-                (loop :for depth :below +cl-optional-cache-size+
-                      :collect (make-cl-optional depth)))
-  :test 'equalp)
-
-(alexandria:define-constant cl-none (aref cl-optional-cache 0) :test 'equalp)
-
 (declaim (inline cl-none-p))
 (defun cl-none-p (x)
-  ;; This would ideally be EQ instead of EQUALP,
-  ;; however, we need to make sure the logic
-  ;; compiles with the standard. For example,
-  ;; if this were EQ, then
-  ;; (CL-NONE-P (READ-FROM-STRING "#.NONE"))
-  ;; would evaluate to false.
-  (equalp x cl-none))
+  (eq x cl-none))
 
 (declaim (inline cl-some))
 (defun cl-some (x)
+  (declare (optimize speed (safety 0) (debug 0)))
   (typecase x
     (cl-optional
-     (let ((new-depth (1+ (cl-optional-depth x))))
-       (if (< new-depth +cl-optional-cache-size+)
-           (aref cl-optional-cache new-depth)
-           (make-cl-optional new-depth))))
+     (make-cl-optional (1+ (depth x))))
     (otherwise
      x)))
 
@@ -87,12 +125,14 @@ value (Some^N None)."
 
 (declaim (inline unwrap-cl-some))
 (defun unwrap-cl-some (x)
+  (declare (optimize speed (safety 0) (debug 0)))
   (typecase x
     (cl-optional
-     (let ((new-depth (1- (cl-optional-depth x))))
-       (if (< new-depth +cl-optional-cache-size+)
-           (aref cl-optional-cache new-depth)
-           (make-cl-optional new-depth))))
+     ;; The Coalton type system should guarantee this never goes below
+     ;; 0. We have seen bugs before, though, where CL-NONE-P fails
+     ;; when the argument *is* CL-NONE, causing the downstream effect
+     ;; of decrementing this below zero. (These bugs arose from the
+     ;; EQ-ness of CL-NONE.)
+     (make-cl-optional (1- (depth x))))
     (otherwise
      x)))
-
