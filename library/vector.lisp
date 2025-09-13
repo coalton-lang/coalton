5c9a8235ac5f1393a7eed5d665ed00f27606f7cb
garlic0x1 <43155857+garlic0x1@users.noreply.github.com>
2025-09-13 04:03:00 -0400

    New Vector Operations (#1644)
    * destructive vec ops
    
    * proper english
    
    * Update vector.lisp
    
    Co-authored-by: Yarin Heffes <yarin.heffes@gmail.com>
    
    * Update vector.lisp
    
    Co-authored-by: Yarin Heffes <yarin.heffes@gmail.com>
    
    * fix nits
    
    ---------
    
    Co-authored-by: Robert Smith <robert@stylewarning.com>
    Co-authored-by: Yarin Heffes <yarin.heffes@gmail.com>

--- library/vector.lisp
+++ library/vector.lisp
@@ -18,11 +18,13 @@
    #:with-initial-element
    #:singleton
    #:length
+   #:subseq
    #:capacity
    #:empty?
    #:singleton?
    #:copy
    #:set-capacity!
+   #:resect!
    #:clear!
    #:push!
    #:pop!
@@ -95,6 +97,18 @@
       (cl:length v)))
 
   (inline)
+  (declare subseq (Vector :a -> UFix -> UFix -> Vector :a))
+  (define (subseq v start end)
+    "Compute a subseq of a vector bounded by given indices.
+
+`start` index is inclusive and `end` index is exclusive."
+    (let ((real-start (min start end))
+          (real-end (min (length v) (max start end))))
+      (lisp (Vector :a) (real-start real-end v)
+        (cl:let ((result (cl:make-array (cl:- real-end real-start) :adjustable cl:t))) 
+          (cl:replace result v :start2 real-start)))))
+
+  (inline)
   (declare capacity (Vector :a -> UFix))
   (define (capacity v)
     "Returns the number of elements that `v` can store without resizing."
@@ -131,6 +145,19 @@
       ;; fill pointer
       (cl:adjust-array v new-capacity :fill-pointer shrinking)
       Unit))
+
+  (inline)
+  (declare resect! (Vector :a -> UFix -> UFix -> Unit))
+  (define (resect! v start end)
+    "Destructively kills a subsequence in a vector bounded by given indices.
+
+`start` index is inclusive and `end` index is exclusive."
+    (let ((real-start (min start end))
+          (real-end (min (length v) (max start end)))
+          (new-size (- (length v) (- real-end real-start))))
+      (lisp (Vector :a) (real-start real-end v)
+        (cl:replace v v :start1 real-start :start2 real-end))
+      (set-capacity! new-size v)))
 
   (inline)
   (declare clear! (Vector :a -> Unit))
