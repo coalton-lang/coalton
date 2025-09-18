8a6cc86f9ddba27d47e795374088f54a0611c8bc
garlic0x1 <garlic1@protonmail.com>
2025-09-18 14:47:28 +0300

    remove token stuff

--- library/stream.lisp
+++ library/stream.lisp
@@ -40,33 +40,6 @@
                types))))
 
 ;;
-;; Tokens
-;;
-
-(coalton-toplevel
-  (define whitespaces
-    (lisp (List UFix) ()
-      (cl:mapcar #'cl:char-code '(#\Space #\Tab #\Linefeed #\Return #\Page))))
-
-  (define-class (Newline :elt)
-    (newline? (:elt -> Boolean)))
-
-  (define-instance (Newline Char)
-    (define (newline? obj) (== obj #\Newline)))
-
-  (define-instance (Newline U8)
-    (define (newline? obj) (== obj 10)))
-
-  (define-class (Whitespace :elt)
-    (whitespace? (:elt -> Boolean)))
-
-  (define-instance (Whitespace Char)
-    (define (whitespace? obj) (list:member (char:char-code obj) whitespaces)))
-
-  (define-instance (Whitespace U8)
-    (define (whitespace? obj) (list:member (into obj) whitespaces))))
-
-;;
 ;; Stream Types
 ;;
 
