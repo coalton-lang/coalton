d25b247623099b7545c1257011df32fa7933c851
c-kloukinas <c.kloukinas@gmail.com>
2025-11-26 15:21:21 +0000

    Introduced new file library/coalton-compat.lisp, so as to collect eventually all non-compatible code inside it, so that other code avoids conditional compilation.
    File library/hash.lisp is a demonstration of its use (uses try-lock-package and
    the new compatible implementation of hash-combine).

---
+++ library/coalton-compat.lisp
@@ -0,0 +1,155 @@
+(cl:defpackage #:coalton-compat
+  (:use #:cl)
+  (:export
+;;;; try-* are macros/functions that most probably will never be
+;;;; implemented by all Lisps.
+;;;;
+;;;; For the rest (not starting with try-), while an implementation
+;;;; may not exist at the moment for some Lisp, it might exist in the
+;;;; future.
+   #:try-lock-package
+   #:try-freeze-type
+   ;; #:hash-combine64
+   #:hash-combine))
+
+(in-package #:coalton-compat)
+
+(defmacro try-lock-package (the-package)
+  #+sb-package-locks
+  `(sb-ext:lock-package ,the-package))
+(defmacro try-unlock-package (the-package)
+  #+sb-package-locks
+  `(sb-ext:unlock-package ,the-package))
+
+(defmacro try-freeze-type (the-type)
+  #+sbcl
+  `(declaim (sb-ext:freeze-type ,the-type)))
+
+(pushnew
+ (if (>= #x7F cl:most-positive-fixnum)
+     (intern "COALTON:8-BIT-FIXNUM" "KEYWORD")
+     (if (>= #x7FFF cl:most-positive-fixnum)
+         (intern "COALTON:16-BIT-FIXNUM" "KEYWORD")
+         (if (>= #x7FFFFFFF cl:most-positive-fixnum)
+             (intern "COALTON:32-BIT-FIXNUM" "KEYWORD")
+             (if (>= #x7FFFFFFFFFFFFFFF cl:most-positive-fixnum)
+                 (intern "COALTON:64-BIT-FIXNUM" "KEYWORD")
+                 (error "Unknown fixnum size")))))
+ cl:*features*)
+
+;;; (defun lisp-combine-hashes (lhs rhs)
+;;;   ;; SBCL has a hash combination function
+;;;   #+sbcl(sb-int:mix lhs rhs))
+;;;
+;;; #+(and |COALTON:32-BIT-FIXNUM| (not sbcl))
+;;;   ;;
+;;;   ;; Generic hash combination functions copied from:
+;;;   ;; https://stackoverflow.com/questions/5889238/why-is-xor-the-default-way-to-combine-hashes/27952689#27952689
+;;;   ;;
+;;;   ;; 32bit hash combination
+;;;   ;; #+(or allegro abcl)
+;;;   (cl:logxor lhs (cl:+ rhs #x9e3779b9 (cl:ash lhs 6) (cl:ash lhs -2))))
+;;;
+;;; #+(and |COALTON:64-BIT-FIXNUM| (not sbcl))
+;;;   ;; #+(or ccl ecl)
+;;;   ;; 64bit hash combination
+;;;   ;; logand required on ccl to force the output to be a fixnum
+;;;   (cl:logand (cl:logxor lhs (cl:+ rhs #x517cc1b727220a95 (cl:ash lhs 6) (cl:ash lhs -2))) cl:most-positive-fixnum))
+
+;; Notes:
+;;
+;; 1) Despite a Lisp having 32/64 bit fixnums, they don't necessarily
+;; use all the bits - sbcl seems to use 63 bits, and ecl 62 bits.
+;;
+;; 2) The above stackoverflow discussion seems to have been critisized
+;; in another one here, discussing an earlier implementation of
+;; boost::hash_combine that used that approach:
+;; https://stackoverflow.com/questions/35985960/c-why-is-boosthash-combine-the-best-way-to-combine-hash-values
+;;
+;; Boost itself uses this approach now:
+;; https://www.boost.org/doc/libs/latest/libs/container_hash/doc/html/hash.html#notes_hash_combine
+;;
+;; And for the definition of combine:
+;; https://www.boost.org/doc/libs/latest/libs/container_hash/doc/html/hash.html#ref_hash_combine
+;;               combine(s, v) = mix(s + 0x9e3779b9 + v)
+;;
+;; So, we'll implement the two (32/64bit) boost::hash_combine
+;; versions, and then choose the one that's closer to the fixnum size
+;; of the current Lisp.
+(defmacro mod-pos (x max)
+  `(declare ((integer) ,x ,max))
+  `(mod (if (> 0 ,x) (- ,x) ,x) ( + 1 ,max)))
+
+(defmacro mix-formula (x k1 k2 k3 m1 m2 max)
+  `(declare (fixnum ,k1 ,k2 ,k3 ,m1 ,m2) ((integer 0 ,max) ,max))
+  "The original C++ code assumes that it operates on an N-bit unsigned integer - thus mod-pos"
+  ;; See https://www.boost.org/doc/libs/latest/libs/container_hash/doc/html/hash.html#notes_hash_combine
+  ;; x ^= x >> k1;
+  ;; x *= m1;
+  ;; x ^= x >> k2;
+  ;; x *= m2;
+  ;; x ^= x >> k3;
+  (let ((round1 (gensym))
+        (round2 (gensym))
+        (round3 (gensym)))
+    `(let* ((,round1 (mod-pos (* (cl:logxor ,x      (cl:ash ,x      ,k1)) ,m1)
+                              ,max))
+            (,round2 (mod-pos (* (cl:logxor ,round1 (cl:ash ,round1 ,k2)) ,m2)
+                              ,max))
+            (,round3 (mod-pos    (cl:logxor ,round2 (cl:ash ,round2 ,k3))
+                                 ,max)))
+            ,round3)))
+
+(defmacro combine-32bit (lhs rhs)
+  `(declare ((integer 0 #xFFFFFFFF) ,lhs ,rhs))
+  `(mix-formula (mod-pos (+ ,lhs #x9e3779b9 ,rhs) #xFFFFFFFF)
+                16 15 15 #x21f0aaad #x735a2d97
+                #xFFFFFFFF))
+
+(defmacro combine-64bit (lhs rhs)
+  `(declare ((integer 0 #xFFFFFFFFFFFFFFFF) ,lhs ,rhs))
+  ;; boost::hash_combine uses #x9e3779b9 for this version too.
+  ;;
+  ;; see definition of has_combine in lines 469-473 of
+  ;; https://github.com/boostorg/container_hash/blob/060d4aea6b5b59d2c9146b7d8e994735b2c0a582/include/boost/container_hash/hash.hpp
+  `(mix-formula (mod-pos (+ ,lhs #x517cc1b727220a95 ,rhs) #xFFFFFFFFFFFFFFFF)
+                32 32 28 #xe9846af9b1a615d #xe9846af9b1a615d
+                #xFFFFFFFFFFFFFFFF))
+
+(defun hash-combine (lhs rhs)
+  #+(or |COALTON:32-BIT-FIXNUM| |COALTON:16-BIT-FIXNUM| |COALTON:8-BIT-FIXNUM|)
+  (declare ((integer 0 #xFFFFFFFF) lhs rhs)) ; may not be fixnums!
+  #-(or |COALTON:32-BIT-FIXNUM| |COALTON:16-BIT-FIXNUM| |COALTON:8-BIT-FIXNUM|)
+  (declare ((integer 0 #xFFFFFFFFFFFFFFFF) lhs rhs)) ; may not be fixnums!
+  "Uses either the 32bit or the 64bit implementation of boost::hash_combine, as these were described on 2025, Oct 11, with constant #x9e3779b9 changed to #xe9846af9b1a615d in the 64bit case"
+  ;; https://web.archive.org/web/20251011141945/https://www.boost.org/doc/libs/latest/libs/container_hash/doc/html/hash.html#notes_hash_combine
+  ;;
+  ;; https://web.archive.org/web/20251011141945/https://www.boost.org/doc/libs/latest/libs/container_hash/doc/html/hash.html#ref_hash_combine#ref_hash_combine
+  ;;
+  ;; https://github.com/boostorg/container_hash/blob/060d4aea6b5b59d2c9146b7d8e994735b2c0a582/include/boost/container_hash/detail/hash_mix.hpp
+  (mod-pos                              ; ensure it's <= most-positive-fixnum
+   #+(or |COALTON:32-BIT-FIXNUM| |COALTON:16-BIT-FIXNUM| |COALTON:8-BIT-FIXNUM|)
+   (combine-32bit lhs rhs)
+   #-(or |COALTON:32-BIT-FIXNUM| |COALTON:16-BIT-FIXNUM| |COALTON:8-BIT-FIXNUM|)
+   (combine-64bit lhs rhs)
+   cl:most-positive-fixnum))
+
+(defun hash-combine64 (lhs rhs)
+  (declare ((integer 0 #xFFFFFFFFFFFFFFFF) lhs rhs)) ; may not be fixnums!
+  (mod-pos (combine-64bit lhs rhs)
+           cl:most-positive-fixnum))
+
+;; For testing
+#|
+(load "coalton-compat.lisp")
+(progn
+  (in-package :cl-user)
+  (format t "~A~%" (macroexpand '(coalton-compat:try-lock-package #:coalton-compat)))
+  (format t "~A~%" (macroexpand '(coalton-compat:try-freeze-type cl:fixnum)))
+  (format t "~A~%" (coalton-compat:hash-combine 0 0))
+  (format t "~A~%" (coalton-compat:hash-combine #xFFFFFFFFFFFFFFFF #xFFFFFFFFFFFFFFFF))
+  (format t "~A~%" (coalton-compat:hash-combine #x7FFFFFFFFFFFFFFF #x7FFFFFFFFFFFFFFF))
+  (format t "~A~%" (coalton-compat:hash-combine #xFFFFFFFF #xFFFFFFFF))
+  (format t "~A~%" (coalton-compat:hash-combine #x7FFFFFFF #x7FFFFFFF))
+  (format t "~A~%" cl:*features*))
+|#
