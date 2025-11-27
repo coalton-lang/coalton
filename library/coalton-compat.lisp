405bca609466cc6ea1791dff00dfe6c9693b3566
c-kloukinas <c.kloukinas@gmail.com>
2025-11-27 15:46:20 +0000

    Fixed indentation in unset-all-float-traps.

--- library/coalton-compat.lisp
+++ library/coalton-compat.lisp
@@ -32,11 +32,11 @@
 
 (defmacro unset-all-float-traps ()
   (cl:eval-when (:compile-toplevel :load-toplevel :execute)
-  #+ccl (ccl:set-fpu-mode :overflow nil :underflow nil :division-by-zero nil :invalid nil :inexact nil)
-  #+sbcl (sb-int:set-floating-point-modes :traps nil)
-  #+abcl (extensions:set-floating-point-modes :traps nil)
-  #+ecl  (ext:trap-fpe 'cl:t nil)
-  ))
+    #+ccl (ccl:set-fpu-mode :overflow nil :underflow nil :division-by-zero nil :invalid nil :inexact nil)
+    #+sbcl (sb-int:set-floating-point-modes :traps nil)
+    #+abcl (extensions:set-floating-point-modes :traps nil)
+    #+ecl  (ext:trap-fpe 'cl:t nil)
+    ))
 
 (pushnew
  (cond ((= 16 (integer-length cl:most-positive-fixnum))
