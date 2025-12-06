842ad2413355e18740c3d2c788ddf28940fd5bf4
Shiro Kawai <shiro@acm.org>
2025-12-06 10:20:41 -1000

    Fix running tests in slime
    In slime REPL, readtable is somewhat messed up once a complete test
    is run, and (asdf:test-system :coalton/tests) fails in the second time.
    
    This is a band-aid to ensure readtables are reset while it matters.
    A proper fix may be done in slime, or there may be a workaround in
    the Coalton side.  Meanwhile, some fix is nice since this is extemely
    annoying while developing in slime.

--- tests/entry-tests.lisp
+++ tests/entry-tests.lisp
@@ -4,10 +4,11 @@
   (test-file "examples/small-coalton-programs/src/fact-fib.coal"))
 
 (defun source-forms (string)
-  (with-input-from-string (stream string)
-    (loop :for form := (read stream nil nil)
-          :while form
-          :collect form)))
+  (let ((*readtable* (copy-readtable nil)))
+    (with-input-from-string (stream string)
+      (loop :for form := (read stream nil nil)
+            :while form
+            :collect form))))
 
 (deftest test-compile-to-lisp ()
   "Test that the Coalton compiler compiles a test file into something that looks like Lisp source."
@@ -28,7 +29,8 @@
       (fmakunbound fact)
       (setf entry:*global-environment* (tc:unset-function entry:*global-environment* fact)))
     (let ((file (source:make-source-file (compile-test-file) :name "test")))
-      (entry:compile file :load t)
+      (let ((*readtable* (copy-readtable nil)))
+        (entry:compile file :load t))
       (let ((fact (test-sym)))
         (is (fboundp fact)
             "Test function was bound as side effect of loading fasl")
