d92b94db585a3bfc5e5e3e7336151c62a09d47d2
Robert Smith <robert@stylewarning.com>
2026-04-25 18:00:20 -0400

    support backward slurp/barf

--- mine/lessons/04-barf.lisp
+++ mine/lessons/04-barf.lisp
@@ -1,7 +1,11 @@
 (
  "If you were thinking there must be an opposite to slurping, you're right, it's barfing. Barfing means to eject the last item in a parenthetical expression. Again, doesn't matter what it is: a single symbol or another parenthetical. It doesn't matter where your cursor is in a particular expression, all that matters is what nesting depth it's at. (Same for slurping too, by the way.)
 
-Motion: (a b| c d) --> press Alt+Left --> (a b| c) d"
+Motion: (a b| c d) --> press Alt+← --> (a b| c) d
+
+Like slurping, the above barfs in the forward direction. We can also barf in the backward direction:
+
+Motion: a (b c|) d --> press Alt+Shift+→ --> a b (c|) d"
 
  ("(a b c d)" . "(a b c) d")
  ("(a b c (d))" . "(a b c) (d)")
