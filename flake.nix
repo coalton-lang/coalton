3d23da4c070ca212b4a1fc8ff5d042b29bc7f5b0
Judah Sotomayor <judah.sotomayor@pm.me>
2026-06-18 23:11:32 -0400

    feat: Enable building for multiple lisps.
    Extract coalton build logic into a function that takes the lisp to build
    with as an input.

--- flake.nix
+++ flake.nix
@@ -9,127 +9,149 @@
 
   outputs = inputs@{ nixpkgs, flake-parts, cl-nix-lite, ... }:
   flake-parts.lib.mkFlake { inherit inputs; } {
-    systems = nixpkgs.lib.platforms.all;
+    systems = [
+      "x86_64-linux"
+      "x86_64-darwin"
+      "aarch64-linux"
+      "aarch64-darwin"
+    ];
 
-    perSystem = { self', config, pkgs, lib, system, ... }: {
-      _module.args.pkgs = import inputs.nixpkgs {
-        inherit system;
-        overlays = [
-          cl-nix-lite.overlays.default
-        ];
-      };
+    imports = [
+      inputs.flake-parts.flakeModules.easyOverlay
+    ];
 
-      packages = with pkgs.lispPackagesLite; rec {
+    perSystem = { self', config, pkgs, lib, system, ... }:
+    let
+      coaltonFor =      # Function to build Coalton for a particular lisp
+        lisp':
+        with pkgs.lispPackagesLiteFor lisp'; rec {
+          source-error = lispDerivation {
+            lispSystem = "source-error";
+            src = ./source-error;
+            lispDependencies = [
+              alexandria
+            ];
+          };
 
-        source-error = lispDerivation {
-          lispSystem = "source-error";
-          src = ./source-error;
-          lispDependencies = [
-            alexandria
-          ];
-        };
+          coalton-compiler = lispDerivation {
+            lispSystem = "coalton-compiler";
+            src = lib.cleanSource ./.;
+            lispDependencies = [
+              alexandria
+              concrete-syntax-tree
+              eclector
+              eclector-concrete-syntax-tree
+              float-features
+              named-readtables
+              source-error
+              trivial-gray-streams
+            ];
+          };
 
-        coalton-compiler = lispDerivation {
-          lispSystem = "coalton-compiler";
-          src = lib.cleanSource ./.;
-          lispDependencies = [
-            alexandria
-            concrete-syntax-tree
-            eclector
-            eclector-concrete-syntax-tree
-            float-features
-            named-readtables
-            source-error
-            trivial-gray-streams
-          ];
-        };
-
-        inherit
-            (lispMultiDerivation {
-              src = lib.cleanSource ./.;
-              systems = {
-                coalton-library = {
-                  lispSystem = "coalton/library";
-                  lispDependencies = [
-                    coalton-compiler
-                    trivial-garbage
-                    alexandria
-                  ];
-                };
-
-                coalton = {
-                  lispDependencies = [
-                    coalton-compiler
-                    coalton-library
-                  ];
-                  lispCheckDependencies = [
-                    fiasco
-                    coalton-examples
-                  ];
-                };
-                
-                coalton-examples = {
-                  lispSystems = [
-                    "quil-coalton"
-                    "small-coalton-programs"
-                    "thih-coalton"
-                  ];
-                  lispDependencies = [
-                    coalton
-                  ];
-                  lispCheckDependencies = [ fiasco ];
-                };
-
-                coalton-xmath = {
-                  lispSystem = "coalton/xmath";
-                  lispDependencies = [
-                    coalton
-                    coalton-library
-                    computable-reals
-                  ];
-                };
-
-                coalton-doc = {
-                  lispSystem = "coalton/doc";
-                  lispDependencies = [
-                    coalton
-                    coalton-xmath
-                    html-entities
-                    yason
-                    spinneret
-                  ];
-                };
+          inherit (lispMultiDerivation {
+            src = lib.cleanSource ./.;
+            systems = {
+              coalton-library = {
+                lispSystem = "coalton/library";
+                lispDependencies = [
+                  coalton-compiler
+                  trivial-garbage
+                  alexandria
+                ];
               };
 
-              propagatedBuildInputs =
-                systems:
-                lib.optionals (builtins.elem "coalton/xmath" systems) [
-                  pkgs.mpfr
+              coalton = {
+                lispDependencies = [
+                  coalton-compiler
+                  coalton-library
                 ];
-              preBuild =
-                let
-                  testDirectories = [
-                    "$PWD/examples/coalton-testing-example-project"
-                    "$PWD/examples/fractal"
-                    "$PWD/examples/quil-coalton"
-                    "$PWD/examples/small-coalton-programs"
-                    "$PWD/examples/thih"
-                  ];
-                  testPaths = lib.concatStringsSep ":" testDirectories;
-                in
-                  ''
-                    export CL_SOURCE_REGISTRY="${testPaths}:$CL_SOURCE_REGISTRY"
-                  '';
-            })
-          coalton
-          coalton-library
-          coalton-doc
-          coalton-xmath
-          coalton-examples
-        ;
-      };
+                lispCheckDependencies = [
+                  fiasco
+                  coalton-examples
+                ];
+              };
+                
+              coalton-examples = {
+                lispSystems = [
+                  "quil-coalton"
+                  "small-coalton-programs"
+                  "thih-coalton"
+                ];
+                lispDependencies = [
+                  coalton
+                ];
+                lispCheckDependencies = [ fiasco ];
+              };
 
-      devShells.default = self'.packages.coalton;
-    };
+              coalton-xmath = {
+                lispSystem = "coalton/xmath";
+                lispDependencies = [
+                  coalton
+                  coalton-library
+                  computable-reals
+                ];
+              };
+
+              coalton-doc = {
+                lispSystem = "coalton/doc";
+                lispDependencies = [
+                  coalton
+                  coalton-xmath
+                  html-entities
+                  yason
+                  spinneret
+                ];
+              };
+            };
+
+            propagatedBuildInputs =
+              systems:
+              lib.optionals (builtins.elem "coalton/xmath" systems) [
+                pkgs.mpfr
+              ];
+            preBuild =
+              let
+                testDirectories = [
+                  "$PWD/examples/coalton-testing-example-project"
+                  "$PWD/examples/fractal"
+                  "$PWD/examples/quil-coalton"
+                  "$PWD/examples/small-coalton-programs"
+                  "$PWD/examples/thih"
+                ];
+                testPaths = lib.concatStringsSep ":" testDirectories;
+              in
+                ''
+                  export CL_SOURCE_REGISTRY="${testPaths}:$CL_SOURCE_REGISTRY"
+                '';
+          })
+            coalton
+            coalton-library
+            coalton-doc
+            coalton-xmath
+            coalton-examples
+          ;
+        };
+          
+    in
+      {
+        _module.args.pkgs = import inputs.nixpkgs {
+          inherit system;
+          overlays = [
+            cl-nix-lite.overlays.default
+          ];
+        };
+
+        # Output an overlay featuring both our packages and the `coaltonFor` function.
+        overlayAttrs = {
+          inherit (config.packages) coalton;
+          inherit coaltonFor;
+        };
+
+        # Output packages built with sbcl
+        packages =
+          coaltonFor pkgs.sbcl;
+
+        devShells.default = self'.packages.coalton;
+      };
   };
 }
